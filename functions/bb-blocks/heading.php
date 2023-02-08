12ba5960f13f3aac57a1e1fc1ee6d37d86231e28
Ingo Schmid <is@bananenbiegerei.de>
2023-02-08 14:59:51 +0100

    headline block with bg color

--- functions/bb-blocks/heading.php
+++ functions/bb-blocks/heading.php
@@ -2,9 +2,9 @@
 
 acf_register_block_type([
 	'name' => 'heading',
-	'title'				=> __('Numero Headline'),
+	'title'				=> __('Headline'),
 	'description'		=> __('Basic Block for Headlines'),
-	'render_template'	=> get_template_directory() . '/template-parts/bb-blocks/headline-block.php',
+	'render_template'	=> get_template_directory() . '/template-parts/bb-blocks/heading.php',
 	'category'			=> 'wkmde-custom-blocks',
 	'icon'				=> 'heading',
 	'keywords'			=> [],
