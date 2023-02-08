468a6e9849e9e8c0fa0ca984394f96df554d18cc
Ingo Schmid <is@bananenbiegerei.de>
2023-02-08 15:25:05 +0100

    fixes heading + paragraph

--- functions/bb-blocks/paragraph.php
+++ functions/bb-blocks/paragraph.php
@@ -4,7 +4,7 @@ acf_register_block_type([
 	'name' => 'paragraph',
 	'title'				=> __(' Paragraph'),
 	'description'		=> __('Basic Block for Paragraphs'),
-	'render_template'	=> get_template_directory() . '/template-parts/bb-blocks/paragraph-block.php',
+	'render_template'	=> get_template_directory() . '/template-parts/bb-blocks/paragraph.php',
 	'category'			=> 'wkmde-custom-blocks',
 	'icon'				=> 'editor-paragraph',
 	'keywords'			=> [],
