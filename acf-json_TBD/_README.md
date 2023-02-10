4ba5227fbbabb1b54467066985c18d15e1ec7989
Éric Leclercq <eric@naad.io>
2023-02-10 13:50:32 +0000

    Cleanup of old ACF fields (not used ones are for no in 'acf-json_TBD’ folder)

---
+++ acf-json_TBD/_README.md
@@ -0,0 +1,1 @@
+This folder contains ACF-JSON files for blocks which have not been properly assessed yet.
--- template-parts/bb-blocks/accordion.php
+++ template-parts/bb-blocks/accordion.php
@@ -1,15 +1,17 @@
-<div class="accordion acf-block-accordion m-5">
-    <ul>
-        <?php while (have_rows('acfb_add_accordion')): ?>
-    	    <?php the_row(); ?>
-            <li>
-                <details>
-                    <summary class="font-alt text-xl py-10 border-b border-gray-200"><?php echo esc_html(get_sub_field('acfb_accordion_title')); ?></summary>
-                    <div class="text-inherit text-base my-9">
-                        <?php the_sub_field('acfb_accordion_content'); ?>
-                    </div>
-                </details>
-            </li>
-        <?php endwhile; ?>
-    </ul>
-</div>
+	<div class="accordion acf-block-accordion m-5">
+		<ul>
+			<?php while (have_rows('acfb_add_accordion')): ?>
+				<?php the_row(); ?>
+				<li>
+					<details>
+						<summary class="font-alt text-xl py-10 border-b border-gray-200">
+							<?php echo esc_html(get_sub_field('acfb_accordion_title')); ?>
+						</summary>
+						<div class="text-inherit text-base my-9">
+							<?php the_sub_field('acfb_accordion_content'); ?>
+						</div>
+					</details>
+				</li>
+			<?php endwhile; ?>
+		</ul>
+	</div>
