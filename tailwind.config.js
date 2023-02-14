28618a36dee2a21851d40fca5417dc6e68a8aed8
Ingo Schmid <is@bananenbiegerei.de>
2023-02-14 15:06:01 +0100

    adjusted font sizes and synced to figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
 			xs: '0.75rem', // 12px
 			sm: '0.875rem', // 14px
 			base: '1rem', // 16px
-			lg: '1.25rem', // 20px
-			xl: '1.5rem', // 24px
-			'2xl': '2rem', // 32px
-			'3xl': '3rem', // 48px
-			'4xl': '3.5rem', // 56px
-			'5xl': '4.375rem', // 60px
+			lg: '1.125rem', // 18px
+			xl: '1.25rem', // 20px
+			'2xl': '1.5rem', // 24px
+			'3xl': '1.875rem', // 30px
+			'4xl': '2.25rem', // 36px
+			'5xl': '3rem', // 48px
 		},
 		fontFamily: {
 			alt: ['Montserrat', 'sans-serif'],
