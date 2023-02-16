f3aae37c90c83ed94d82219e1349341776538ade
Éric Leclercq <eric@naad.io>
2023-02-16 17:13:07 +0000

    Continued work on image block (added hover caption)

--- template-parts/bb-blocks/image.php
+++ template-parts/bb-blocks/image.php
@@ -2,44 +2,32 @@
 $image = get_field('image');
 $size = 'full';
 $ratio_factor = 0.74; // height compared to a width of 1. Example 4:3 -> .75, or .74 to allow some leeway
-if ($image):
 
-	$image_meta_data = wp_get_attachment_metadata($image);
-	if ($image_meta_data['width'] * $ratio_factor > $image_meta_data['height']) {
-		$image_class = 'landscape';
-	} else {
-		$image_class = 'portrait';
-	}
-	$image_caption = wp_get_attachment_caption($image);
-	?>
-<?php
-$width = get_field('options');
-if ($width == 'narrow') {
-	$width = 'max-w-2xl mx-auto';
+$image_meta_data = wp_get_attachment_metadata($image);
+if ($image_meta_data['width'] * $ratio_factor > $image_meta_data['height']) {
+	$format = 'landscape';
+} else {
+	$format = 'portrait';
 }
+
+$image_caption = wp_get_attachment_caption($image);
+$width = 'max-w-2xl mx-auto';
+if (get_field('wide')) {
+	$width = '';
+}
+$figure_classes = $format == 'landscape' ? 'relative my-2 rounded-2xl' : 'flex rounded-2xl relative justify-center bg-secondary my-2 overflow-hidden';
+$image_classes = ['class' => ($format == 'landscape' ? 'rounded-2xl w-full h-auto' : 'z-10 h-auto max-w-2xl mx-auto w-full') . " {$width}"];
 ?>
 	<div class="bb-image-block <?php echo $width; ?>">
-		<?php if ($image_class == 'landscape'): ?>
-			<figure class="relative my-2" role="group">
-				<?php echo wp_get_attachment_image($image, $size, '', ['class' => 'w-full h-auto']); ?>
+
+			<figure class="<?= $figure_classes ?>" role="group">
+				<?= wp_get_attachment_image($image, $size, '', $image_classes) ?>
 				<?php if ($image_caption): ?>
-					<figcaption class="caption hidden lg:block absolute top-0 right-0 bg-secondary w-auto h-auto z-20 p-2 text-sm">
-						<?php echo esc_attr($image_caption); ?>
+					<figcaption
+						class="invisible flex absolute rounded-b-2xl absolute left-0 bottom-0 right-0 text-white bg-black w-auto h-auto z-20 p-2 text-sm flex items-start gap-4 break-all"
+						>
+						<?= bb_icon('info') ?> <div><?= strip_tags($image_caption, ['a']) ?></div>
 					</figcaption>
 				<?php endif; ?>
 			</figure>
-		<?php else: ?>
-			<figure class="flex justify-center bg-secondary my-2" role="group">
-				<div class="relative">
-					<?php echo wp_get_attachment_image($image, $size, '', ['class' => 'h-auto max-w-2xl mx-auto w-full']); ?>
-					<?php if ($image_caption): ?>
-						<figcaption class="block caption absolute top-0 right-0 bg-secondary w-auto h-auto z-20 p-2 text-sm">
-							<?php echo esc_attr($image_caption); ?>
-						</figcaption>
-					<?php endif; ?>
-				</div>
-			</figure>
-		<?php endif; ?>
 	</div>
-<?php
-endif; ?>
