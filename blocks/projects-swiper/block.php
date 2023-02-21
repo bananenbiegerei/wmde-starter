ef17e74b8ae237d50100ad8fe4f2a64635606519
Ingo Schmid <is@bananenbiegerei.de>
2023-02-21 18:01:38 +0100

    refurbished projects swiper

--- blocks/projects-swiper/block.php
+++ blocks/projects-swiper/block.php
@@ -11,84 +11,104 @@
 <?php $slide_bg = get_field('background') == 'white' ? 'bg-gray-100' : ' bg-white'; ?>
 
 <?php if (!is_admin()): ?>
-	<div class="bb-projects-swiper-block px-10 pb-16 pt-16 <?= $swiper_bg ?>" id="<?= $block['id'] ?>">
-			<h2 class="text-5xl"><?= __('Projects', BB_TEXT_DOMAIN) ?></h2>
-
-			<div class="swiper-container relative">
-
-				<!-- Navigation -->
-				<div class="absolute -top-20 right-0 hidden lg:block">
-						<?= bb_icon('swiper-left', 'swiper-button-prev h-14 w-14 hover:text-transparent cursor-pointer') ?>
-						<?= bb_icon('swiper-right', 'swiper-button-next h-14 w-14 hover:text-transparent cursor-pointer') ?>
-				</div>
-
-				<!-- Slides -->
-				<div class="swiper-wrapper">
-					<?php foreach (get_field('projects') as $project): ?>
-						<div class="swiper-slide rounded-2xl p-8 pt-16 <?= $slide_bg ?>"><!-- Slide size defined in block SCSS -->
-							<a class="block flex flex-col items-center" href="<?php echo get_post_permalink($project->ID); ?>">
-
-								<!-- Thumbnail -->
-								<div class="mb-10">
-										<?php if (has_post_thumbnail($project->ID)): ?>
-											<img
-												class="h-64 object-contain"
-												src="<?php echo get_the_post_thumbnail_url($project->ID, 'medium'); ?>">
-										<?php endif; ?>
-								</div>
-
-								<!-- Title -->
-								<div class="mb-2">
-										<h3 class="text-3xl"><?php echo $project->post_title; ?></h3>
-								</div>
-
-								<!-- Description -->
-								<div class="text-lg text-inherit">
-									<?php if (get_field('description', $project->ID)): ?>
-										<p><?php echo wp_trim_words(get_field('description', $project->ID), EXCERPT_LENGTH, '... <br>'); ?></p>
-									<?php else: ?>
-										<p><?php echo get_the_excerpt($project->ID); ?></p>
-									<?php endif; ?>
-								</div>
-							</a>
-						</div>
-					<?php endforeach; ?>
-				</div>
-			</div>
+<div class="bb-projects-swiper-block container <?= $swiper_bg ?>" id="<?= $block['id'] ?>">
+	<div class="flex">
+		<h2 class="text-5xl flex-1"><?= __('Projects', BB_TEXT_DOMAIN) ?></h2>
+		<!-- Navigation -->
+		<div class="relative hidden lg:block">
+			<?= bb_icon('chevron-left', 'swiper-button-prev btn btn-icon-only btn-hollow cursor-pointer') ?>
+			<?= bb_icon('chevron-right', 'swiper-button-next btn btn-icon-only btn-hollow cursor-pointer') ?>
+		</div>
 	</div>
-	<script>
-			SwipersConfig['#<?= $block['id'] ?>'] = {
-					loop: true,
-					slidesPerView: 'auto',
-					centeredSlides: true,
-					spaceBetween: 40, // FIXME: should be rem?
-					grabCursor: true,
-					draggable: true,
-					navigation: {
-							nextEl: '#<?= $block['id'] ?> .swiper-button-next',
-							prevEl: '#<?= $block['id'] ?> .swiper-button-prev',
-					},
-					speed: 1400,
-					autoplay: {
-							delay: 4000,
-							disableOnInteraction: true,
-					},
-					breakpoints: {}
-			};
-	</script>
+	<div class="swiper-container relative">
+		<!-- Slides -->
+		<div class="swiper-wrapper h-full">
+			<?php foreach (get_field('projects') as $project): ?>
+			<div class="swiper-slide rounded-2xl p-5 <?= $slide_bg ?> self-stretch"><!-- Slide size defined in block SCSS -->
+				<a class="block flex flex-col space-y-5" href="<?php echo get_post_permalink($project->ID); ?>">
+					<!-- Thumbnail -->
+					<div>
+						<?php if (has_post_thumbnail($project->ID)): ?>
+						<img class="h-40 object-contain mx-auto my-5" src="<?php echo get_the_post_thumbnail_url($project->ID, 'medium'); ?>">
+						<?php endif; ?>
+					</div>
+
+					<!-- Title -->
+					<div>
+						<h3 class="text-3xl break-words font-alt font-normal"><?php echo $project->post_title; ?></h3>
+					</div>
+
+					<!-- Description -->
+					<div class="text-lg text-inherit">
+						<?php if (get_field('description', $project->ID)): ?>
+						<p><?php echo wp_trim_words(get_field('description', $project->ID), EXCERPT_LENGTH, '... <br>'); ?></p>
+						<?php else: ?>
+						<p><?php echo get_the_excerpt($project->ID); ?></p>
+						<?php endif; ?>
+					</div>
+				</a>
+			</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+</div>
+<script>
+	SwipersConfig['#<?= $block['id'] ?>'] = {
+		loop: true,
+		slidesPerView: '1',
+		centeredSlides: false,
+		spaceBetween: 40,
+		grabCursor: true,
+		draggable: true,
+		navigation: {
+			nextEl: '#<?= $block['id'] ?> .swiper-button-next',
+			prevEl: '#<?= $block['id'] ?> .swiper-button-prev',
+		},
+		speed: 1400,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: true,
+		},
+		breakpoints: {
+			// when window width is >= 320px
+			320: {
+			  slidesPerView: 1,
+			  spaceBetween: 20
+			},
+			// when window width is >= 480px
+			480: {
+			  slidesPerView: 1,
+			  spaceBetween: 30
+			},
+			// when window width is >= 640px
+			640: {
+			  slidesPerView: 2,
+			  spaceBetween: 40
+			},
+			1028: {
+			  slidesPerView: 3,
+			  spaceBetween: 40
+			},
+			1200: {
+			  slidesPerView: 4,
+			  spaceBetween: 40
+			}
+		  }
+	};
+</script>
 <?php else: ?>
-	<!-- Skeleton view for editor -->
-	<div class="<?= $swiper_bg ?>">
+<!-- Skeleton view for editor -->
+<div class="<?= $swiper_bg ?>">
 	<b><?= __('Projects Swiper', BB_TEXT_DOMAIN) ?></b>
 
 	<div class="grid grid-cols-4 gap-4">
 		<?php foreach (get_field('projects') as $project): ?>
-			<div class="rounded-3xl p-4 <?= $slide_bg ?>">
-				<?php if (has_post_thumbnail($project->ID)): ?>
-					<img class="h-32 object-contain" src="<?php echo get_the_post_thumbnail_url($project->ID, 'medium'); ?>">
-				<?php endif; ?>
-				<p class="text-s"><?php echo $project->post_title; ?></p>
-			</div>
+		<div class="rounded-3xl p-4 <?= $slide_bg ?>">
+			<?php if (has_post_thumbnail($project->ID)): ?>
+			<img class="h-32 object-contain" src="<?php echo get_the_post_thumbnail_url($project->ID, 'medium'); ?>">
+			<?php endif; ?>
+			<p class="text-s"><?php echo $project->post_title; ?></p>
+		</div>
 		<?php endforeach; ?>
 	</div>
-<?php endif; ?>
+	<?php endif; ?>
