2b7fc2031cb9feca4e720f5e2b3951972f2c5479
Ingo Schmid <is@bananenbiegerei.de>
2023-02-22 12:49:16 +0100

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer class="bg-white" aria-labelledby="footer-heading">
 	<h2 id="footer-heading" class="sr-only">Footer</h2>
-	<div class="border-y border-primary-600 py-8">
+	<div class="border-t-2 border-b-2 border-b-gray-100 border-primary-600 py-8">
 		<div class="container lg:flex lg:gap-20">
 			<div class="flex-none">
 				<?php get_template_part("template-parts/social-media-menu"); ?>
@@ -10,24 +10,32 @@
 				<?php get_template_part("template-parts/newsletter-signup-form-minimal"); ?>
 			</div>
 			<?php if( get_field('kontakt_spendenservice', 'option') ): ?>
-				<div  class="flex-none">
-					<?php the_field('kontakt_spendenservice', 'option', false, false); ?>
+				<div  class="flex-none font-alt">
+					<h6 class="text-base text-primary"><?= _e('Spendenservice', BB_TEXT_DOMAIN) ?></h6>
+					<?php the_field('kontakt_spendenservice', 'option'); ?>
 				</div>
 			<?php endif; ?>
 			<?php if( get_field('kontakt', 'option') ): ?>
-				<div  class="flex-none">
-					<?php the_field('kontakt', 'option', false, false); ?>
+				<div  class="flex-none font-alt">
+					<h6 class="text-base text-primary"><?= _e('Kontakt', BB_TEXT_DOMAIN) ?></h6>
+					<?php the_field('kontakt', 'option'); ?>
 				</div>
 			<?php endif; ?>
 		</div>
 	</div>
-	<div class="container flex items-center">
-		<?php wp_nav_menu(array(
-		'container' => 'nav',
-		'menu' => 'footer',
-		'menu_class' => 'menu horizontal',
-		'theme_location' => 'footer',
-		)); ?>
+	<div class="container flex items-center h-24">
+		<div class="flex-1">
+			<?php wp_nav_menu(array(
+			'container' => 'nav',
+			'menu' => 'footer',
+			'menu_class' => 'menu horizontal',
+			'theme_location' => 'footer',
+			)); ?>
+		</div>
+		
+		<div class="flex-none">
+			<h6 class="mb-0 text-base">Wir befreien Wissen</h6>
+		</div>
 	</div>
 </footer>
 
