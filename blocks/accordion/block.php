9f17cf29cc9d7f276827f2ec8d4892601e975ea9
Ingo Schmid <is@bananenbiegerei.de>
2023-02-27 12:43:06 +0100

    accordion tailwind setup

--- blocks/accordion/block.php
+++ blocks/accordion/block.php
@@ -1,13 +1,13 @@
-<div class="bb-accordion-block m-5">
+<div class="bb-accordion-block">
 	<ul>
 		<?php while (have_rows('acfb_add_accordion')): ?>
 		<?php the_row(); ?>
 		<li>
 			<details>
-				<summary class="font-alt text-xl py-10 border-b border-gray-200">
+				<summary class="font-alt text-xl py-7 cursor-pointer border-b border-gray-200 pr-10">
 					<?php echo esc_html(get_sub_field('acfb_accordion_title')); ?>
 				</summary>
-				<div class="text-inherit text-base my-9">
+				<div class="text-inherit text-base my-7 text-xl">
 					<?php the_sub_field('acfb_accordion_content'); ?>
 				</div>
 			</details>
