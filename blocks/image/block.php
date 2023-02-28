d21277d3f7707eebffae13d7a711593c37a6a184
Éric Leclercq <eric@naad.io>
2023-02-28 08:38:38 +0000

    Placeholder when image is missing in Image block

--- blocks/image/block.php
+++ blocks/image/block.php
@@ -8,19 +8,26 @@ $image_id = get_field('image') ? get_field('image') : $args['image']['id'] ?? fa
 // If called from Wikimedia Commons Media block
 $wmc_image_data = $args['wmc_data'] ?? false;
 
-if ($wmc_image_data) {
+if ($image_id) {
+	$image_caption = wp_get_attachment_caption($image_id);
+	$image_descripton = get_post($image_id)->post_content;
+	$image_meta_data = wp_get_attachment_metadata($image_id);
+	$wide = get_field('wide');
+} elseif ($wmc_image_data) {
 	$image_caption = $wmc_image_data['usageterms'] . ' - <a href="' . esc_attr($wmc_image_data['url']) . '">Wikimedia Commons</a>';
 	$image_descripton = $wmc_image_data['desc'];
 	$dim = explode('x', $wmc_image_data['dim']);
-	$image_meta_data = ['width' => $dim[0], 'height' => $dim[1]];
+	$image_meta_data = ['width' => (int) $dim[0], 'height' => (int) $dim[1]];
+	$wide = $args['wide'];
 } else {
-	$image_caption = wp_get_attachment_caption($image);
-	$image_descripton = get_post($image)->post_content;
-	$image_meta_data = wp_get_attachment_metadata($image);
+	$image_caption = 'Missing image!';
+	$image_descripton = '';
+	$image_meta_data = ['width' => 180, 'height' => 139];
+	$wide = false;
 }
 
-$width = get_field('wide');
 // Default values
+$width = $wide ? 'FIXME:missing' : 'FIXME:missing';
 $figure_classes = 'flex flex-col relative rounded-2xl';
 $image_classes = ['class' => "{$width} rounded-2xl w-full h-auto"];
 // For vertical images
@@ -29,15 +36,18 @@ if ($image_meta_data['width'] * 0.74 < $image_meta_data['height']) {
 	$image_classes = ['class' => "{$width} h-auto max-w-2xl mx-auto w-full"];
 }
 
-if ($wmc_image_data) {
+if ($image_id) {
+	$image = wp_get_attachment_image($image_id, 'full', '', $image_classes);
+} elseif ($wmc_image_data) {
 	$image = "<img src='{$wmc_image_data['media_url']}' srcset='{$wmc_image_data['srcset']}' alt='{$wmc_image_data['desc']}' loading='lazy' sizes='(max-width: 1024px) 100vw, 1024px' class='{$image_classes['class']}'>";
 } else {
-	$image = wp_get_attachment_image($image_id, 'full', '', $image_classes);
+	$placeholder = esc_url(get_template_directory_uri() . '/blocks/image/placeholder.svg');
+	$image = "<img src='{$placeholder}' class='{$image_classes['class']}'>";
 }
 
 $figure_classes .= $image_caption ? '' : ' no_caption';
 ?>
-<div class="bb-image-block my-4 <?php echo $width; ?>">
+<div class="bb-image-block my-4 <?= $width ?>">
 	<figure class="<?= $figure_classes ?> col-span-12 lg:col-span-8 lg:col-start-3" role="group">
 		<?= $image ?>
 		<?php if ($image_caption): ?>
