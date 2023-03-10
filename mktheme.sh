04a1a1435001502c96129ff410be177aa1764086
Éric Leclercq <eric@naad.io>
2023-03-10 13:24:25 +0000

    Fix to breadcrumbs

--- mktheme.sh
+++ mktheme.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+# Creates a zip file of the theme ready to upload to WordPress
 
 cd ..
 rm -f wmde.zip
-zip wmde.zip wmde -rv -x wmde/node_modules/\* -x wmde/package\*.json -x wmde/src/\* wmde/gulpfile.js -x wmde/.git\* -x  wmde/mktheme.sh -x wmde/acf-json_TBD/\* -x wmde/blocks.disabled/\* -x wmde/.env\* -x \*/.DS_Store -x wmde/.nova/\* 
+zip wmde.zip wmde -rv -x wmde/node_modules/\* -x wmde/package\*.json -x wmde/src/\* wmde/gulpfile.js -x wmde/.git\* -x  wmde/mktheme.sh -x wmde/acf-json_TBD/\* -x wmde/blocks.disabled/\* -x wmde/.env\* -x \*/.DS_Store -x wmde/.nova/\* -x wmde/README.md
--- template-parts/breadcrumb.php
+++ template-parts/breadcrumb.php
@@ -6,27 +6,18 @@ if (is_front_page() || is_404()) {
 
 global $post;
 $breadcrumbs = [];
-
-if (is_archive()) {
-	$breadcrumbs[] = '<li>' . post_type_archive_title('', false) . '</li>';
-} elseif (is_page()) {
-	if ($post->post_parent) {
-		$anc = get_post_ancestors($post->ID);
-		$anc = array_reverse($anc);
-		if (!isset($parents)) {
-			$parents = null;
-		}
-		foreach ($anc as $ancestor) {
-			$breadcrumbs[] = '<li><a href="' . get_permalink($ancestor) . '">' . get_the_title($ancestor) . '</a></li>';
-		}
-		echo $parents;
-		$breadcrumbs[] = '<li>' . bb_icon('caret-right', 'text-red-500') . get_the_title() . '</li>';
-	} else {
-		$breadcrumbs[] = '<li>' . bb_icon('caret-right', 'text-red-500') . get_the_title() . '</li>';
+if ($post->post_parent) {
+	$anc = get_post_ancestors($post->ID);
+	$anc = array_reverse($anc);
+	foreach ($anc as $k => $ancestor) {
+		$breadcrumbs[] = '<li>' . ($k > 0 ? bb_icon('caret-right', 'text-red-500') : '') . '<a href="' . get_permalink($ancestor) . '">' . get_the_title($ancestor) . '</a></li>';
 	}
 } elseif (get_post_type() == 'projects') {
 	$breadcrumbs[] = '<li><a href="' . get_post_type_archive_link('projects') . '">' . __('Projects') . '</a></li>';
-	$breadcrumbs[] = '<li>' . bb_icon('caret-right', 'text-red-500') . get_the_title() . '</li>';
+}
+
+if (count($breadcrumbs) == 0) {
+	return;
 }
 ?>
 
