fe29b1eca1b43333a527d1c77f80dba9d6e23ed1
Ingo Schmid <is@bananenbiegerei.de>
2023-03-16 12:12:50 +0100

    styled breadcrumb

--- template-parts/breadcrumbs.php
+++ template-parts/breadcrumbs.php
@@ -10,7 +10,7 @@ if ($post->post_parent) {
 	$anc = get_post_ancestors($post->ID);
 	$anc = array_reverse($anc);
 	foreach ($anc as $k => $ancestor) {
-		$breadcrumbs[] = '<li>' . ($k > 0 ? bb_icon('caret-right', 'text-red-500') : '') . '<a href="' . get_permalink($ancestor) . '">' . get_the_title($ancestor) . '</a></li>';
+		$breadcrumbs[] = '<li>' . ($k > 0 ? bb_icon('caret-right', 'text-red-500 relative top-1') : '') . '<a href="' . get_permalink($ancestor) . '">' . get_the_title($ancestor) . '</a></li>';
 	}
 } elseif (get_post_type() == 'projects') {
 	$breadcrumbs[] = '<li><a href="' . get_post_type_archive_link('projects') . '">' . __('Projects') . '</a></li>';
@@ -22,7 +22,7 @@ if (count($breadcrumbs) == 0) {
 ?>
 
 <div class="flex items-center h-16 mb-6">
-	<ul class="menu horizontal breadcrumbs">
+	<ul class="flex gap-1 items-baseline">
 		<?= join('', $breadcrumbs) ?>
 	</ul>
 </div>
