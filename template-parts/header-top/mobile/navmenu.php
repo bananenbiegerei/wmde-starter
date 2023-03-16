561ca5185d6e39ab6316138cd43c4b54926304f9
Ingo Schmid <is@bananenbiegerei.de>
2023-03-16 13:52:13 +0100

    added some animation to mobile menu

--- template-parts/header-top/mobile/navmenu.php
+++ template-parts/header-top/mobile/navmenu.php
@@ -38,7 +38,7 @@
 				</div>
 
 				<!-- Wrapper for domain items -->
-				<div x-show="isOpen[i]">
+				<div x-show="isOpen[i]" x-transition:enter="transition ease-out duration-300" x-transition:enter-start="opacity-0 transform scale-90" x-transition:enter-end="opacity-100 transform scale-100" x-transition:leave="transition ease-in duration-300" x-transition:leave-start="opacity-100 transform scale-100" x-transition:leave-end="opacity-0 transform scale-90">
 
 					<!-- Domain link -->
 					<!-- <div>
