ae607b4c379b46195936e382ff18f1a7b0ec86c6
Ingo Schmid <is@bananenbiegerei.de>
2023-03-21 16:59:28 +0100

    styling for team members

--- blocks/team-members/team-members.php
+++ blocks/team-members/team-members.php
@@ -7,7 +7,8 @@ foreach (get_field('team_members') as $member_id) {
 		'email' => get_field('email', $member_id),
 		'label_for_related_project' => get_field('label_for_related_project', $member_id),
 		'related_project' => get_field('related_project', $member_id) ? get_field('related_project', $member_id) : [],
-		'photo' => get_the_post_thumbnail($member_id, 'medium', null),
+		'photo' => get_the_post_thumbnail($member_id, 'thumbnail', array('class' => 'rounded-full  max-w-max w-full')),
+		
 	];
 	$member['related_project_ext'] = [];
 	while (have_rows('related_project_ext', $member_id)) {
@@ -17,28 +18,30 @@ foreach (get_field('team_members') as $member_id) {
 	$members[] = $member;
 }
 ?>
-		<div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+		<div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
 				<?php foreach ($members as $member): ?>
-					<div>
-							<?= $member['photo'] ?>
-							<div class="font-alt text-base text-inherit mt-2">
-									<h4><?= $member['name'] ?></h4>
-									<p class="margin-bottom-0 font-size-h4"><?= $member['details'] ?></p>
-									<p><a href="mailto:<?= $member['email'] ?>">E-mail</a></p>
+							<div class="space-y-2 sm:flex sm:gap-5">
+								<div class="overflow-hidden basis-1/3 self-start">
+									<?= $member['photo'] ?>
+								</div>
+								<div class="basis-2/3">
 									<?php $related_project = $member['related_project']; ?>
 									<?php $related_project_ext = $member['related_project_ext']; ?>
 									<?php if ($related_project || $related_project_ext): ?>
 										<?= $member['label_for_related_project'] ?>
 										<ul>
 											<?php foreach ($related_project as $post): ?>
-												<li><a href="<?= get_permalink($post->ID) ?>"><?= get_the_title($post->ID) ?></a></li>
+												<li><a class="uppercase text-sm text-primary font-bold text-sm font-alt" href="<?= get_permalink($post->ID) ?>"><?= get_the_title($post->ID) ?></a></li>
 											<?php endforeach; ?>
 											<?php foreach ($related_project_ext as $title => $url): ?>
-												<li><a href="<?= $url ?>"><?= $title ?></a></li>
+												<li><a class="uppercase text-xs text-primary font-bold text-sm font-alt" href="<?= $url ?>"><?= $title ?></a></li>
 											<?php endforeach; ?>
 										</ul>
 									<?php endif; ?>
+										<h4 class="text-lg"><?= $member['name'] ?></h4>
+										<p class="margin-bottom-0 text-lg"><?= $member['details'] ?></p>
+										<p class="text-lg"><a href="mailto:<?= $member['email'] ?>">E-mail</a></p>
+								</div>
 							</div>
-					</div>
 				<?php endforeach; ?>
 		</div>
