693da725612f1804103d153fb77bc70833a9012f
Ingo Schmid <is@bananenbiegerei.de>
2023-03-23 12:12:27 +0000

    nicer accordions

--- blocks/accordion/accordion.php
+++ blocks/accordion/accordion.php
@@ -4,10 +4,10 @@
 		<?php the_row(); ?>
 		<li>
 			<details>
-				<summary class="font-alt text-xl py-7 cursor-pointer border-b border-gray-200 pr-10">
+				<summary class="font-alt text-xl py-6 cursor-pointer border-b border-gray-200 pr-10">
 					<?php echo esc_html(get_sub_field('acfb_accordion_title')); ?>
 				</summary>
-				<div class="text-inherit text-base my-7 text-xl">
+				<div class="my-5 accordion-content">
 					<?php the_sub_field('acfb_accordion_content'); ?>
 				</div>
 			</details>
