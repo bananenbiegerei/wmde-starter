52b655a23864f8c74b452338048ddc8bbc54ac46
Éric Leclercq <eric@naad.io>
2023-03-27 13:56:40 +0000

    misc

--- blocks/button/button.php
+++ blocks/button/button.php
@@ -4,12 +4,13 @@ $style = get_field('display')['style'];
 $size = get_field('display')['size'];
 $position = get_field('display')['position'];
 $color = get_field('display')['color'] ?? 'primary';
+// FIXME: button colors not defined yet!
 $icon = get_field('display')['icon'];
 $icon_size = 'icon-' . explode('-', $size)[1];
 ?>
 
-<div id="<?= $block['id'] ?>" class="bb-button-block flex <?= $position ?>">
-	<a class="btn <?= $size ?> btn-<?= $color ?> <?= $style ?> <?= $icon ? 'btn-icon-left' : '' ?>" href="<?= esc_url($link['url']) ?>" target="<?= esc_attr($link['target']) ?>">
+<div id="<?= $block['id'] ?>" class="bb-button-block flex <?= $position ?> <?= $size ?> <?= $icon ? 'btn-icon-left' : '' ?>">
+	<a class="btn <?= $size ?> btn-<?= $color ?> <?= $style ?>" href="<?= esc_url($link['url']) ?>" target="<?= esc_attr($link['target']) ?>">
 		<?= bb_icon($icon, $icon_size) ?>
 		<?= esc_html($link['title']) ?>
 	</a>
