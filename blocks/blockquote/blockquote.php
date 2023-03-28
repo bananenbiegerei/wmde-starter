50988140debb1a46206d5ee7bd56511698936fbd
Ingo Schmid <is@bananenbiegerei.de>
2023-03-28 13:48:01 +0200

    new layout blockquote

--- blocks/blockquote/blockquote.php
+++ blocks/blockquote/blockquote.php
@@ -1,9 +1,13 @@
-<div class="bb-blockquote-block relative">
-  <blockquote class="text-xl lg:text-3xl leading-tight font-normal ml-32 mt-0 mb-5">
-    <?= get_field('text') ?>
-  </blockquote>
-  <?= bb_icon('quote', 'absolute top-0 left-8 icon-xxl') ?>
-  <?php if (get_field('source')): ?>
-    <cite class="ml-32 font-normal text-gray-400 block"><?= get_field('source') ?></cite>
-  <?php endif; ?>
+<div class="bb-blockquote-block relative flex gap-5">
+  <div class="flex-none">
+    <?= bb_icon('quote', 'icon-xxl') ?>
+  </div>
+  <div class="flex-1">
+    <blockquote class="text-xl lg:text-3xl leading-tight font-normal mb-5">
+      <?= get_field('text') ?>
+    </blockquote>
+    <?php if (get_field('source')): ?>
+      <cite class="font-normal text-gray-400 block"><?= get_field('source') ?></cite>
+    <?php endif; ?>
+  </div>
 </div>
