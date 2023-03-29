894419f8003bbd1d48c5805d6b927c261fe9a744
Ingo Schmid <is@bananenbiegerei.de>
2023-03-29 11:23:18 +0000

    cosmetic fixes to team cards

--- blocks/team-members/team-members.php
+++ blocks/team-members/team-members.php
@@ -7,7 +7,7 @@ foreach (get_field('team_members') as $member_id) {
 		'email' => get_field('email', $member_id),
 		'label_for_related_project' => get_field('label_for_related_project', $member_id),
 		'related_project' => get_field('related_project', $member_id) ? get_field('related_project', $member_id) : [],
-		'photo' => get_the_post_thumbnail($member_id, 'thumbnail', array('class' => 'rounded-full  max-w-max w-full')),
+		'photo' => get_the_post_thumbnail($member_id, 'thumbnail', array('class' => 'rounded-lg  max-w-max w-full')),
 		
 	];
 	$member['related_project_ext'] = [];
@@ -20,21 +20,21 @@ foreach (get_field('team_members') as $member_id) {
 ?>
 		<div class="grid grid-cols-1 sm:grid-cols-2 gap-5">
 				<?php foreach ($members as $member): ?>
-							<div class="space-y-2 sm:flex sm:gap-5">
-								<div class="overflow-hidden basis-1/3 self-start">
+							<div class="sm:flex sm:gap-5">
+								<div class="overflow-hidden basis-1/4 self-start">
 									<?= $member['photo'] ?>
 								</div>
-								<div class="basis-2/3">
+								<div class="basis-3/4 -translate-y-1">
 									<?php $related_project = $member['related_project']; ?>
 									<?php $related_project_ext = $member['related_project_ext']; ?>
 									<?php if ($related_project || $related_project_ext): ?>
 										<?= $member['label_for_related_project'] ?>
 										<ul>
 											<?php foreach ($related_project as $post): ?>
-												<li><a class="uppercase text-sm text-primary font-bold text-sm font-alt" href="<?= get_permalink($post->ID) ?>"><?= get_the_title($post->ID) ?></a></li>
+												<li><a class="uppercase text-sm text-primary font-bold text-sm font-alt mb-2" href="<?= get_permalink($post->ID) ?>"><?= get_the_title($post->ID) ?></a></li>
 											<?php endforeach; ?>
 											<?php foreach ($related_project_ext as $title => $url): ?>
-												<li><a class="uppercase text-xs text-primary font-bold text-sm font-alt" href="<?= $url ?>"><?= $title ?></a></li>
+												<li><a class="uppercase text-xs text-primary font-bold text-sm font-alt mb-2" href="<?= $url ?>"><?= $title ?></a></li>
 											<?php endforeach; ?>
 										</ul>
 									<?php endif; ?>
