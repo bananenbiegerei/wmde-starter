a57ea40ada96f40e19f31ed722127b1c5eb253d8
Ingo Schmid <is@bananenbiegerei.de>
2023-04-12 15:46:27 +0200

    title bar with nicer buttons

--- template-parts/header-top/titlebar_content.php
+++ template-parts/header-top/titlebar_content.php
@@ -12,10 +12,10 @@
 		<?php if (get_field('link_fur_spenden', 'option')): ?>
 		<li>
 			<a
-				class="btn btn-red btn-hollow btn-ghost btn-base btn-icon-left"
+				class="btn btn-red btn-hollow btn-sm btn-icon-left"
 				target="_blank"
 				href="<?php echo esc_url(get_field('link_fur_spenden', 'option')); ?>">
-				<?= bb_icon('heart', 'heartbeat') ?>
+				<?= bb_icon('heart', 'heartbeat icon-sm') ?>
 				<?php pll_e('donate'); ?>
 			</a>
 		</li>
@@ -23,7 +23,7 @@
 		<?php if (get_field('link_fur_mitmachen', 'option')): ?>
 		<li class="hidden lg:block">
 			<a
-				class="btn btn-ghost btn-base"
+				class="btn btn-ghost btn-sm"
 				target="_blank"
 				href="<?php echo esc_url(get_field('link_fur_mitmachen', 'option')); ?>">
 				<?php pll_e('Mitmachen'); ?>
