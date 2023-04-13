0eb011dad2033375c611eecaace266c6a3a9ffac
Ingo Schmid <is@bananenbiegerei.de>
2023-04-13 12:42:46 +0000

    added template part for page headers

--- page.php
+++ page.php
@@ -1,42 +1,9 @@
 <?php get_header(); ?>
 <?php while (have_posts()): ?>
 <?php the_post(); ?>
-	<?php if (has_post_thumbnail()): ?>
-	  <div class="bg-gray min-h-[12rem]">
-		  <div class="container grid grid-cols-12">
-			  <div class="col-span-12 lg:col-span-10 lg:col-start-2">
-				  <?php get_template_part('template-parts/breadcrumbs'); ?>
-					<h1 class="text-2xl lg:text-5xl my-5"><?php the_title(); ?></h1>
-					<?php if (has_excerpt()): ?>
-					  <div class="font-alt text-2xl font-normal mb-10">
-						<?php echo strip_tags(get_the_excerpt()); ?>
-					  </div>
-					<?php endif; ?>
-			  </div>
-			<div class="col-span-12 lg:col-span-8 lg:col-start-3">
-				<?php get_template_part('template-parts/featured-image'); ?>
-			</div>
-		  </div>
-	  </div>
-	<?php else: ?>
-	  <div class="bg-gray rounded-b-3xl min-h-[12rem] pb-10">
-		  <div class="container grid grid-cols-12">
-			  <div class="col-span-12 lg:col-span-8 lg:col-start-3">
-				  <?php get_template_part('template-parts/breadcrumbs'); ?>
-					<h1 class="text-5xl my-5"><?php the_title(); ?></h1>
-					<?php if (has_excerpt()): ?>
-					  <div class="font-alt text-xl font-normal">
-						<?php the_excerpt(); ?>
-					  </div>
-					<?php endif; ?>
-			  </div>
-		  </div>
-	  </div>
-	<?php endif; ?>
-	<?php //get_template_part('template-parts/anchor-nav'); ?>
+	<?php get_template_part('template-parts/page-header'); ?>
 	<div class="content pt-10">
 		<?php the_content(); ?>
 	</div>
-
 <?php endwhile; ?>
 <?php get_footer(); ?>
