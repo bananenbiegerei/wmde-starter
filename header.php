58b62a7218faaa540da62b8e28c113870e7f699a
Ingo Schmid <is@bananenbiegerei.de>
2023-04-13 11:47:29 +0000

    make the footer at bottom of viewport with little content

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="h-full no-js" data-theme="wmde">
 <?php get_template_part('head'); ?>
-<body <?php body_class(); ?>>
+<body <?php body_class('flex flex-col min-h-screen'); ?>>
   <?php get_template_part('template-parts/header-top/main'); ?>
-  <main class="main-content min-h-screen pt-12 lg:pt-0">
+  <main class="main-content flex-grow pt-12 lg:pt-0">
