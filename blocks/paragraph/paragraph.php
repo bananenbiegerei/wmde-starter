a71bc532f0ec7960f0c093be16895a4c1ec155d0
Ingo Schmid <is@bananenbiegerei.de>
2023-04-13 12:42:32 +0000

    fix for paragraph size mess

--- blocks/paragraph/paragraph.php
+++ blocks/paragraph/paragraph.php
@@ -6,9 +6,18 @@ if ($text_color) {
 	$text_color = "text-{$text_color}";
 }
 
-$text_size = get_field('style_text_size');
+//$text_size = get_field('style_text_size');
+$text_size_query = get_field('style_text_size');
+  if ($text_size_query== 'small') {
+	$text_size = 'text-base';
+  } elseif ($text_size_query == 'text-base lg:text-xl') {
+	$text_size = 'text-base';
+  } elseif ($text_size_query == 'large') {
+	$text_size = 'text-xl lg:text-2xl';
+  } else {
+	$text_size = 'text-base lg:text-xl';
+  }
 ?>
-
 <div class="bb-paragraph-block mb-5 font-inherit <?= $text_color ?> <?= $text_size ?>">
 		<?= get_field('paragraph') ?>
 </div>
