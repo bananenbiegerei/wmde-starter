0b772ddc37ee8c145d57723f3b63288cfcc47715
Ingo Schmid <is@bananenbiegerei.de>
2023-04-13 15:51:25 +0000

    new press release card

--- blocks/latest-press-releases/latest-press-releases.php
+++ blocks/latest-press-releases/latest-press-releases.php
@@ -4,10 +4,16 @@ $count = get_field('show_all') ? -1 : get_field('count');
 $posts = get_posts(['post_type' => 'press-releases', 'numberposts' => $count]);
 ?>
 
-<div class="bb-latest-press-releases-block">
-	<div class="container grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-8">
+<div class="bb-latest-press-releases-block mb-20">
+	<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mb-5">
 			<?php foreach ($posts as $p): ?>
-				<?php get_template_part('blocks/card/card', null, ['blog_id' => $blog_id, 'post_id' => $p->ID, 'layout' => 'vne', 'placeholder' => '/blocks/latest-press-releases/placeholder.svg']); ?>
-		<?php endforeach; ?>
+				<?php include( locate_template( 'template-parts/card-press-release.php', false, false ) ); ?>
+			<?php endforeach; ?>
 	</div>
+	<?php if ( get_field( 'show_all' ) ): ?>	
+	<?php else: ?>
+		<a class="btn btn-base" href="<?php echo get_page_link('pressemittelungen'); ?>">
+			<?= _e('Alle Pressemitteilungen', BB_TEXT_DOMAIN) ?>
+		</a>
+	<?php endif; ?>
 </div>
---
+++ template-parts/card-press-release.php
@@ -0,0 +1,4 @@
+<div class="rounded-xl bg-gray p-5">
+  <?php $post_title = get_the_title($p->ID); ?>
+  <h2 class="text-base"><?php echo $post_title; ?></h2>
+</div>
