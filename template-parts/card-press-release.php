ca5d467a2019d9361f5e166e121c4b79c43fe37a
Ingo Schmid <is@bananenbiegerei.de>
2023-04-14 04:34:11 +0000

    nicer press release card

--- template-parts/card-press-release.php
+++ template-parts/card-press-release.php
@@ -1,4 +1,8 @@
-<div class="rounded-xl bg-gray p-5">
-  <?php $post_title = get_the_title($p->ID); ?>
-  <h2 class="text-base"><?php echo $post_title; ?></h2>
+<?php 
+$post_title = get_the_title($p->ID); 
+$post_date = get_the_date('d.m.Y', $p->ID);
+?>
+<div class="rounded-xl bg-gray p-5 flex flex-col divide-y">
+  <p class="text-sm"><?php echo $post_date; ?></p>
+  <h2 class="text-base pt-2"><?php echo $post_title; ?></h2>
 </div>
