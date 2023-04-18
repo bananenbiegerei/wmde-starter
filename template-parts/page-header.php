80675cd8845ec3cf92436c4a8634d98ea9ba6f26
Ingo Schmid <is@bananenbiegerei.de>
2023-04-18 13:25:05 +0000

    page header with cta buttons

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -20,6 +20,18 @@ $excerpt_style = 'font-alt text-2xl font-normal';
 					<?php echo strip_tags(get_the_excerpt()); ?>
 				  </div>
 				<?php endif; ?>
+				<?php if ( have_rows( 'call_to_actions_in_header' ) ) : ?>
+					  <div class="mb-10">
+					  <?php while ( have_rows( 'call_to_actions_in_header' ) ) : the_row(); ?>
+						  <?php $cta_link = get_sub_field( 'cta_link' ); ?>
+						  <?php if ( $cta_link ) : ?>
+							  <a class="btn btn-base btn-icon-left" href="<?php echo esc_url( $cta_link['url'] ); ?>" target="<?php echo esc_attr( $cta_link['target'] ); ?>">
+							  <?= bb_icon('arrow-right', 'icon-base'); ?>
+							  <?php echo esc_html( $cta_link['title'] ); ?></a>
+						  <?php endif; ?>
+					  <?php endwhile; ?>
+					  </div>
+				  <?php endif; ?>
 		  </div>
 		<div class="col-span-12 lg:col-span-8 lg:col-start-3">
 			<?php get_template_part('template-parts/featured-image'); ?>
@@ -33,11 +45,24 @@ $excerpt_style = 'font-alt text-2xl font-normal';
 			  <?php get_template_part('template-parts/breadcrumbs'); ?>
 				<h1 class="<?php echo $title_style; ?>"><?php the_title(); ?></h1>
 				<?php if (has_excerpt()): ?>
-				  <div class="<?php echo $excerpt_style; ?>">
+				  <div class="<?php echo $excerpt_style; ?> mb-5">
 					<?php the_excerpt(); ?>
 				  </div>
 				<?php endif; ?>
+				<?php if ( have_rows( 'call_to_actions_in_header' ) ) : ?>
+					  <div class="">
+					  <?php while ( have_rows( 'call_to_actions_in_header' ) ) : the_row(); ?>
+						  <?php $cta_link = get_sub_field( 'cta_link' ); ?>
+						  <?php if ( $cta_link ) : ?>
+							  <a class="btn btn-base btn-icon-left" href="<?php echo esc_url( $cta_link['url'] ); ?>" target="<?php echo esc_attr( $cta_link['target'] ); ?>">
+							  <?= bb_icon('arrow-right', 'icon-base'); ?>
+							  <?php echo esc_html( $cta_link['title'] ); ?></a>
+						  <?php endif; ?>
+					  <?php endwhile; ?>
+					  </div>
+				  <?php endif; ?>
 		  </div>
+		  
 	  </div>
   </div>
 <?php endif; ?>
