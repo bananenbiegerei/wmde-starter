2e449e4d098120c1fd4dfdb8e356c021ce9a590e
Ingo Schmid <is@bananenbiegerei.de>
2023-04-25 09:16:55 +0000

    responsive design spacer

--- blocks/spacer/spacer.php
+++ blocks/spacer/spacer.php
@@ -3,19 +3,19 @@ $height = ''; // set default value
 
 switch (get_field('size')) {
 	case 'xs':
-		$height = 'h-4';
+		$height = 'h-2 lg:h-4';
 		break;
 	case 'sm':
-		$height = 'h-6';
+		$height = 'h-3 lg:h-6';
 		break;
 	case 'base':
-		$height = 'h-10';
+		$height = 'h-5 lg:h-10';
 		break;
 	case 'lg':
-		$height = 'h-20';
+		$height = 'h-10 lg:h-20';
 		break;
 	case 'xl':
-		$height = 'h-40';
+		$height = 'h-20 lg:h-40';
 		break;
 }
 ?>
