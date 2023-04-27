4b5ba5664dd1edbb4c048a467b40fca80caf0276
Ingo Schmid <is@bananenbiegerei.de>
2023-04-27 08:47:01 +0200

    link fixes to theme block

--- blocks/theme/theme.php
+++ blocks/theme/theme.php
@@ -1,7 +1,7 @@
 <?php
 $theme = get_field('theme');
 $color = get_field('color_for_theme', $theme->ID);
-$related = get_field('related');
+$related = get_field('related_links');
 $thumbnail_url = get_the_post_thumbnail_url($theme, 'medium');
 $theme_url = get_the_permalink($theme);
 ?>
@@ -10,7 +10,7 @@ $theme_url = get_the_permalink($theme);
 	<div class="rounded-3xl lg:px-10 lg:grid lg:grid-cols-12 overflow-hidden" style="background-color: <?= $color ?>;">
 		<!-- Image -->
 			<div class="lg:col-span-4">
-				<div class="aspect-w-4 aspect-h-3 relative -lg:translate-x-10 rounded-tl-3xl rounded-br-3xl overflow-hidden">
+				<div class="aspect-w-4 aspect-h-3 relative lg:-translate-x-10 rounded-tl-3xl rounded-br-3xl overflow-hidden">
 					<?php
 					  echo '<img class="w-full h-full object-cover" src="' . $thumbnail_url . '" />';
 					?>
@@ -40,79 +40,24 @@ $theme_url = get_the_permalink($theme);
 				</div>
 
 			</div>
-			
-			<?php if ( get_field( 'internal_external_link' ) ): ?>
-			
-			<?php if ( have_rows( 'external_links' ) ) : ?>
-				<div class="col-span-12">
+	
+			<!-- Related -->
+			<?php if ( have_rows( 'related_links' ) ) : ?>
+				<div class="col-span-12 mt-5 mb-10">
 				<div class="lg:grid lg:grid-cols-3">
-				<?php while ( have_rows( 'external_links' ) ) : the_row(); ?>
-					<?php $external_link = get_sub_field( 'external_link' ); ?>
-					
-					<?php if ( $external_link ) : ?>
-						<div class="py-5">
-						<a href="<?php echo esc_url( $external_link['url'] ); ?>" target="<?php echo esc_attr( $external_link['target'] ); ?>">
+				<?php while ( have_rows( 'related_links' ) ) : the_row(); ?>
+					<?php $link = get_sub_field( 'link' ); ?>
+					<?php if ( $link ) : ?>
+						<a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>">
 						<h3 class="text-base lg:text-xl text-bg-related">
-							<?php echo esc_html( $external_link['title'] ); ?>
+						<?php echo esc_html( $link['title'] ); ?>
 						</h3>
 						</a>
-						</div>
 					<?php endif; ?>
-					
 				<?php endwhile; ?>
 				</div>
 				</div>
 			<?php endif; ?>
-			
-			<?php else: ?>
-			
-			<!-- Related -->
-			<?php if ($related): ?>
-				<div class="col-span-12">
-				<div class="lg:grid lg:grid-cols-3">
-				<?php foreach ($related as $related): ?>
-					<?php setup_postdata($related); ?>
-					<div class="py-5">
-						<?php if ($terms = get_the_terms($related->ID, 'theme')): ?>
-						  <?php $term_names = []; ?>
-						  <?php /* prettier-ignore */ foreach ($terms as $term) { $term_names[] = $term->name; } ?>
-						  <div class="topline"><?php echo implode(', ', $term_names); ?></div>
-						 <?php endif; ?>
-						<a href="<?php the_permalink(); ?>">
-							<h3 class="text-base lg:text-xl text-bg-related"><?php echo get_the_title($related->ID); ?></h3>
-						</a>
-					</div>
-				<?php endforeach; ?>
-				</div>
-				</div>
-				<?php wp_reset_postdata(); ?>
-			<?php endif; ?>
-			
-			<?php endif; ?>
 
 	</div>
 </div>
-<!-- <script>
-  // Get all elements with the class "text-inherit"
-  const elements = document.querySelectorAll('.text-bg-related');
-
-  // Loop through all elements and adjust their text color
-  elements.forEach(element => {
-	// Get the background color of the element
-	const bgColor = window.getComputedStyle(element.parentElement).getPropertyValue('background-color');
-
-	// Convert the color to an RGB value
-	const rgbColor = bgColor.match(/\d+/g);
-
-	// Calculate the relative luminance of the color
-	const relativeLuminance = (0.2126 * rgbColor[0] + 0.7152 * rgbColor[1] + 0.0722 * rgbColor[2]) / 255;
-
-	// Set the text color based on the relative luminance
-	if (relativeLuminance > 0.5) {
-	  element.style.color = '#000'; // Set text color to black
-	} else {
-	  element.style.color = '#fff'; // Set text color to white
-	}
-  });
-</script> -->
-
