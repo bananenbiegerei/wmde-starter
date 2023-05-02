00f29c841e315676f5c0568fbcde75732598d59d
Ingo Schmid <is@bananenbiegerei.de>
2023-05-02 09:42:05 +0000

    theme white type

--- blocks/theme/theme.php
+++ blocks/theme/theme.php
@@ -4,10 +4,17 @@ $color = get_field('color_for_theme', $theme->ID);
 $related = get_field('related_links');
 $thumbnail_url = get_the_post_thumbnail_url($theme, 'medium');
 $theme_url = get_the_permalink($theme);
+if( get_field('white_type') ):
+	$type_color = 'text-white';
+endif;
 ?>
-
 <div class="bb-theme-block mb-10 lg:mb-20">
-	<div class="rounded-3xl lg:px-10 lg:grid lg:grid-cols-12 overflow-hidden" style="background-color: <?= $color ?>;">
+	<div class="rounded-3xl lg:px-10 lg:grid lg:grid-cols-12 overflow-hidden
+	<?php if( get_field('white_type') ): ?>
+		<?php echo $type_color; ?>
+	<?php endif; ?>
+	"
+	style="background-color: <?= $color ?>;">
 		<!-- Image -->
 			<div class="lg:col-span-4">
 				<div class="aspect-w-4 aspect-h-3 relative lg:-translate-x-10 rounded-tl-3xl rounded-br-3xl overflow-hidden">
@@ -19,8 +26,9 @@ $theme_url = get_the_permalink($theme);
 			<div class="lg:col-span-8 flex flex-col p-5 lg:p-0">
 				<div class="pt-8">
 					<!-- Theme or format -->
-					<div class="topline">
+					<div class="uppercase font-bold font-alt text-sm <?php echo $type_color; ?>">
 						<?= __('Theme', BB_TEXT_DOMAIN) ?>
+						
 					</div>
 					<!-- Title -->
 					<h2 class="text-2xl lg:text-3xl text-bg-related"><?= esc_html($theme->post_title) ?></h2>
@@ -43,12 +51,17 @@ $theme_url = get_the_permalink($theme);
 	
 			<!-- Related -->
 			<?php if ( have_rows( 'related_links' ) ) : ?>
-				<div class="col-span-12 mt-5 mb-10">
+				<div class="col-span-12 my-10">
 				<div class="lg:grid lg:grid-cols-3">
 				<?php while ( have_rows( 'related_links' ) ) : the_row(); ?>
 					<?php $link = get_sub_field( 'link' ); ?>
 					<?php if ( $link ) : ?>
 						<a href="<?php echo esc_url( $link['url'] ); ?>" target="<?php echo esc_attr( $link['target'] ); ?>">
+						<?php if( get_sub_field('alt_meta_info') ): ?>
+							<p class="uppercase font-bold font-alt text-sm <?php echo $type_color; ?>">
+								<?php the_sub_field('alt_meta_info'); ?>
+							</p>
+						<?php endif; ?>
 						<h3 class="text-base lg:text-xl text-bg-related">
 						<?php echo esc_html( $link['title'] ); ?>
 						</h3>
