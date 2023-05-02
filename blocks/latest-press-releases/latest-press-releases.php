95a8409a2901984bf5313be183b3331e0ef10d3c
Ingo Schmid <is@bananenbiegerei.de>
2023-05-02 07:59:27 +0000

    added text image float

--- blocks/latest-press-releases/latest-press-releases.php
+++ blocks/latest-press-releases/latest-press-releases.php
@@ -3,7 +3,6 @@ $blog_id = get_current_blog_id();
 $count = get_field('show_all') ? -1 : get_field('count');
 $posts = get_posts(['post_type' => 'press-releases', 'numberposts' => $count]);
 ?>
-
 <div class="bb-latest-press-releases-block mb-20">
 	<div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 mb-5">
 			<?php foreach ($posts as $p): ?>
--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -30,11 +30,11 @@
 	  </div>
   </div>
 <?php else: ?>
-  <div class="pb-10">
+  <div class="pb-10 bg-gray rounded-b-3xl">
 	  <div class="container grid grid-cols-12">
 		  <div class="col-span-12 pt-5">
 			  <?php get_template_part('template-parts/breadcrumbs'); ?>
-				<h1 class="<?php echo $title_style; ?>"><?php the_title(); ?></h1>
+				<h1><?php the_title(); ?></h1>
 				<?php if (has_excerpt()): ?>
 				  <div class="font-alt text-2xl font-normal mb-5">
 					<?php the_excerpt(); ?>
