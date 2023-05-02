1370e888214f2fcf5195f3ed7749df5234b3857f
Ingo Schmid <is@bananenbiegerei.de>
2023-05-02 16:02:22 +0200

    RD: Projects Single

--- single-projects.php
+++ single-projects.php
@@ -1,24 +1,27 @@
 <?php get_header(); ?>
 <?php while (have_posts()): ?>
 	<?php the_post(); ?>
-	<div class="bg-gray min-h-[12rem] py-10">
-		  <div class="container grid grid-cols-12 gap-10">
-			  <div class="col-span-12 lg:col-span-3">
-					<div class="flex justify-center mt-4">
+	<div class="bg-gray min-h-[12rem] py-3 lg:py-10">
+		  <div class="container lg:grid lg:grid-cols-12 lg:gap-10">
+			  <div class="lg:col-span-12">
+				  <?php get_template_part('template-parts/breadcrumbs'); ?>
+			  </div>
+			  <div class="lg:col-span-3">
+					<div class="flex lg:justify-center mt-4">
 						  <?php the_post_thumbnail('large', ['class' => 'max-h-[12rem] w-auto max-w-[12rem] h-auto']); ?>
 					</div>
 			  </div>
-			  <div class="col-span-12 lg:col-span-9">
-					<h1 class="<?php echo $title_style; ?> my-5"><?php the_title(); ?></h1>
+			  <div class="lg:col-span-9">
+					<h1 class="my-5"><?php the_title(); ?></h1>
 					<?php if (has_excerpt()): ?>
-					  <div class="<?php echo $excerpt_style; ?> mb-10 text-2xl font-alt font-normal">
+					  <div class="<?php echo $excerpt_style; ?> mb-10 text-xl lg:text-2xl font-alt font-normal">
 						<?php echo strip_tags(get_the_excerpt()); ?>
 					  </div>
 					<?php endif; ?>
 			  </div>
 			
 		  </div>
-	  </div>
+	</div>
 
 	<div class="content pt-10 container lg:grid lg:grid-cols-12 gap-10">
 		<div class="lg:col-span-3 font-alt font-normal flex flex-col space-y-5">
