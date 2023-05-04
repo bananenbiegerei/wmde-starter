c887873e61663e3634e2f7b77894290687ac2143
Ingo Schmid <is@bananenbiegerei.de>
2023-05-04 17:56:41 +0000

    fixed not closing div in default author

--- single.php
+++ single.php
@@ -10,16 +10,19 @@ $portrait = get_field('portrait', 'user_' . $author_id);
 <?php if (has_post_thumbnail()): ?>
 <div class="bg-gray min-h-[12rem] pb-10">
 	<div class="container lg:grid lg:grid-cols-12 lg:gap-10">
+		
 		<div class="lg:col-span-10 lg:col-start-2">
 			<?php get_template_part('template-parts/categories-tags'); ?>
 			<h1 class="my-5 lg:mb-0"><?php the_title(); ?></h1>
 		</div>
 
 		<?php if (has_excerpt()): ?>
-		<div class="<?php echo $excerpt_style; ?> lg:text-xl lg:col-span-5 lg:col-start-2">
+			<div class="<?php echo $excerpt_style; ?> lg:text-xl lg:col-span-5 lg:col-start-2">
 			<?php echo strip_tags(get_the_excerpt()); ?>
 		</div>
 		<?php endif; ?>
+		
+		
 		<div class="lg:col-span-5 my-5 lg:my-0">
 			<div class="bb-image-block aspect-w-16 aspect-h-9 bg-gray-200 rounded-xl overflow-hidden">
 				<figure class="w-full w-full">
@@ -32,57 +35,58 @@ $portrait = get_field('portrait', 'user_' . $author_id);
 				</figure>
 			</div>
 		</div>
+		
+		
 		<div class="col-span-8 lg:col-span-10 lg:col-start-2">
 			<div class="flex gap-10 mb-2">
-				
-			
-			<?php if (get_field('custom_authors')): ?>
-				<?php while (have_rows('custom_authors')): ?>
-				<?php the_row(); ?>
-					<div class="flex flex-col gap-1">
-						<?php 
-						$image = get_sub_field('portrait');
-						$size = 'thumb';
-						if( $image ) { ?>
-							<div class="w-24 h-24 rounded overflow-hidden">
-								<div class="aspect-w-1 aspect-h-1">
-									<?php echo wp_get_attachment_image( $image, $size, "", ["class" => "w-full h-full object-cover"] ); ?>
+				<?php if (get_field('custom_authors')): ?>
+					<?php while (have_rows('custom_authors')): ?>
+					<?php the_row(); ?>
+						<div class="flex flex-col gap-1">
+							<?php 
+							$image = get_sub_field('portrait');
+							$size = 'thumb';
+							if( $image ) { ?>
+								<div class="w-24 h-24 rounded overflow-hidden">
+									<div class="aspect-w-1 aspect-h-1">
+										<?php echo wp_get_attachment_image( $image, $size, "", ["class" => "w-full h-full object-cover"] ); ?>
+									</div>
 								</div>
-							</div>
-						<?php }
-						?>
-						<p class="mb-0 font-bold text-sm">
-							<?php the_sub_field('author'); ?>
-						</p>
-						
-					</div>
-				<?php endwhile; ?>
-			</div>
-				<p class="text-base">
-					<?php echo get_the_date(); ?>
-				</p>
-			<?php else: ?>
-				<div class="flex flex-col gap-1">
-					<div class="w-24 h-24 rounded overflow-hidden">
-					<div class="aspect-w-1 aspect-h-1">
-						<?php if ($portrait) {
-							$portrait_image = $portrait['url'];
-							$portrait_alt = $portrait['alt'];
+							<?php }
 							?>
-							<img class="w-full h-full object-cover" src="<?php echo esc_url($portrait_image); ?>" alt="<?php echo esc_attr($portrait_alt); ?>">
-							<?php
-						} 
-						?>
-					</div>
-					</div>
-					<p class="mb-0 font-bold text-sm">
-						<?php the_author(); ?>
-					</p>
+							<p class="mb-0 font-bold text-sm">
+								<?php the_sub_field('author'); ?>
+							</p>
+							
+						</div>
+					<?php endwhile; ?>
+				</div>
 					<p class="text-base">
 						<?php echo get_the_date(); ?>
 					</p>
-				</div>
-			<?php endif; ?>
+				<?php else: ?>
+					<div class="flex flex-col gap-1">
+						<div class="w-24 h-24 rounded overflow-hidden">
+						<div class="aspect-w-1 aspect-h-1">
+							<?php if ($portrait) {
+								$portrait_image = $portrait['url'];
+								$portrait_alt = $portrait['alt'];
+								?>
+								<img class="w-full h-full object-cover" src="<?php echo esc_url($portrait_image); ?>" alt="<?php echo esc_attr($portrait_alt); ?>">
+								<?php
+							} 
+							?>
+						</div>
+						</div>
+						<p class="mb-0 font-bold text-sm">
+							<?php the_author(); ?>
+						</p>
+						<p class="text-base">
+							<?php echo get_the_date(); ?>
+						</p>
+					</div>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
 </div>
