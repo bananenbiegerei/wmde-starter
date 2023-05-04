3a6fef8870ebd0167473562502a625e14f4d1dfb
Éric Leclercq <eric@naad.io>
2023-05-04 14:06:27 +0000

    Removed dependencies on PLL

--- template-parts/header-top/titlebar_content.php
+++ template-parts/header-top/titlebar_content.php
@@ -1,6 +1,6 @@
 <header class="flex w-full items-center">
 	<div class="flex-1">
-		<?php if ( is_home() ): ?>
+		<?php if (is_home()): ?>
 		<!-- for blog -->
 		<a href="https://blog.wikimedia.de/" class="hidden lg:block" aria-labelledby="site-name">
 			<img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/wikimedia-logo.svg" alt="Wikimedia Logo">
@@ -8,20 +8,20 @@
 		<a href="https://blog.wikimedia.de/" class="block lg:hidden" aria-labelledby="site-name">
 			<img class="w-6 h-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/img/wikimedia-logo-mini.svg" alt="Wikimedia Logo">
 		</a>
-		
+
 		<?php else: ?>
-		
+
 		<a href="<?php echo get_home_url(); ?>" class="hidden lg:block" aria-labelledby="site-name">
 			<img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/wikimedia-logo.svg" alt="Wikimedia Logo">
 		</a>
 		<a href="<?php echo get_home_url(); ?>" class="block lg:hidden" aria-labelledby="site-name">
 			<img class="w-6 h-auto" src="<?php echo get_stylesheet_directory_uri(); ?>/img/wikimedia-logo-mini.svg" alt="Wikimedia Logo">
 		</a>
-		
+
 		<?php endif; ?>
-		
+
 	</div>
-	
+
 	<div class="flex gap-5">
 		<ul class="flex items-center space-x-2 lg:space-x-5 mb-0">
 			<?php if (get_field('link_fur_spenden', 'option')): ?>
@@ -44,10 +44,6 @@
 			</li>
 			<?php endif; ?>
 		</ul>
-		<?php /* 
-		<ul class="menu language-switcher">
-		<?php pll_the_languages( array( 'show_flags' => 0,'show_names' => 1, 'hide_current' => 1, 'hide_if_no_translation' => 1) ); ?>
-		</ul> */?>
 	</div>
 
 </header>
