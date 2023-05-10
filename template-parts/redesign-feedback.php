496cafde473c57f726379601ccbbe267e7f047cb
Ingo Schmid <is@bananenbiegerei.de>
2023-05-10 13:48:11 +0200

    feedback form again

--- template-parts/redesign-feedback.php
+++ template-parts/redesign-feedback.php
@@ -8,7 +8,7 @@
   <div class="fixed bottom-10 right-10 bg-neon rounded-xl p-5 drop-shadow-xl z-50 flex flex-col justify-center max-w-sm" x-data="{ close: true }" x-show="close">
 	  <div class="flex gap-5">
 		  <p class="mb-0">
-			  Wie gefällt Ihnen unser ReDesign? Ihre Meinung ist uns wichtig.
+			  Wie gefällt Ihnen unser Redesign? Ihre Meinung ist uns wichtig.
 		  </p>
 		  <div>
 			  <button x-on:click="close = ! close">
