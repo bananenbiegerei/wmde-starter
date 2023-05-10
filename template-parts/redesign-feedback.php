e6f4497274f014082861563eb561ea735de4dabd
Ingo Schmid <is@bananenbiegerei.de>
2023-05-10 10:20:57 +0000

    Feedback Form mit WP Form

--- template-parts/redesign-feedback.php
+++ template-parts/redesign-feedback.php
@@ -1,31 +1,29 @@
-<div class="fixed bottom-10 right-10 bg-neon rounded-xl p-5 drop-shadow-xl z-50 flex flex-col justify-center max-w-sm" x-data="{ open: false, close: true }" x-show="close">
-	<div class="flex gap-5">
-		<p>
-			Wie gefällt Ihnen unser ReDesign? Ihre Meinung ist uns wichtig.
-		</p>
-		<div>
-			<button x-on:click="close = ! close">
-			<span class="sr-only">
-				<?php _e('schliessen'); ?>
-			</span>	
-			<?= bb_icon('x', 'icon-xs'); ?>
-			</button>
-		</div>
-	</div>
-	<button x-on:click="open = ! open" x-bind:class="{ 'rotate-180': open }">
-		<span class="sr-only">
-			<?php _e('öffnen'); ?>
-		</span>	
-		<?= bb_icon('chevron-down-circle', 'icon-xl'); ?>
-	</button>
-	
-	<div class="bg-neon-100 p-5 rounded-xl mt-2" x-show="open">
-		<label class="block mb-2">
-		  <span class="text-gray-700">Ihre Nachricht</span>
-		  <textarea class="form-textarea mt-1 block w-full h-24" rows="3"></textarea>
-		</label>
-		<button class="btn btn-sm">
-			Absenden
-		</button>
-	</div>
+<div x-data="{ showObject: false }" x-show="showObject" x-init="() => {
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > (window.innerHeight / 2)) {
+			showObject = true;
+		}
+	});
+}">
+  <div class="fixed bottom-10 right-10 bg-neon rounded-xl p-5 drop-shadow-xl z-50 flex flex-col justify-center max-w-sm" x-data="{ close: true }" x-show="close">
+	  <div class="flex gap-5">
+		  <p class="mb-0">
+			  Wie gefällt Ihnen unser ReDesign? Ihre Meinung ist uns wichtig.
+		  </p>
+		  <div>
+			  <button x-on:click="close = ! close">
+			  <span class="sr-only">
+				  <?php _e('schliessen'); ?>
+			  </span>	
+			  <?= bb_icon('x', 'icon-xs'); ?>
+			  </button>
+		  </div>
+	  </div>
+
+	  <div>
+		  <?php
+		  echo do_shortcode('[wpforms id="59443" title="false"]');
+		  ?>
+	  </div>
+  </div>
 </div>
