efc06e6ffc35bde8557d2a382f18504e52011b1e
Éric Leclercq <eric@naad.io>
2023-05-12 12:48:26 +0000

    Workaround for pagination bug

--- front-page.php
+++ front-page.php
@@ -1,9 +1,17 @@
-<?php get_header(); ?>
 <?php
-while ( have_posts() ) : the_post(); ?>
-	<div class="content mt-10">
-		<?php the_content(); ?>
-	</div>
-<?php endwhile;
+/*
+NOTE: This implements a workaround for the pagination bug with Polylang. 'is_page() == true' checks that we're loading the front-page. If it's false it means we're loading '/page/X'...
+*/
 ?>
-<?php get_footer(); ?>
+<?php if (is_page()): ?>
+	<?php get_header(); ?>
+	<?php while (have_posts()): ?>
+		<?php the_post(); ?>
+		<div class="content mt-10">
+			<?php the_content(); ?>
+		</div>
+	<?php endwhile; ?>
+	<?php get_footer(); ?>
+<?php else: ?>
+	 <?php get_template_part('index'); ?>
+<?php endif; ?>
