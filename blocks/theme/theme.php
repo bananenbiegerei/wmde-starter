bfb5cdfdcd191de856797a987fda0a38531d0e8a
Éric Leclercq <eric@naad.io>
2023-05-22 11:10:07 +0200

    Fix to theme error when no theme selected

--- blocks/theme/theme.php
+++ blocks/theme/theme.php
@@ -1,6 +1,11 @@
 <?php
 $theme = get_field('theme');
-// FIXME: handle error if $theme is null
+
+if (!$theme) {
+	echo '<p>Missing Theme</p>';
+	return;
+}
+
 $color = get_field('color_for_theme', $theme->ID);
 $secondary_color = get_field('secondary_color', $theme->ID);
 $color_contrast = get_field('has_dark_background_color', $theme->ID);
@@ -12,17 +17,12 @@ $theme_url = get_the_permalink($theme);
 ?>
 
 <div class="bb-theme-block mb-10 lg:mb-20">
-	<div class="rounded-3xl lg:px-10 lg:grid lg:grid-cols-12 overflow-hidden
-	<?php if ($color_contrast):
- 	echo 'white-scheme';
- endif; ?>
-	"
-	style="background-color: <?= $color ?>;">
+	<div class="rounded-3xl lg:px-10 lg:grid lg:grid-cols-12 overflow-hidden <?= $color_contrast ? 'white-scheme' : '' ?>" style="background-color: <?= $color ?>;">
 		<!-- Image -->
 			<div class="lg:col-span-4">
 				<div class="aspect-w-4 aspect-h-3 relative lg:-translate-x-10 rounded-tl-3xl rounded-br-3xl overflow-hidden">
-					<a href="<?php echo $theme_url; ?>">
-					<img class="w-full h-full object-cover" src="<?php echo $thumbnail_url; ?>" alt="<?php echo $thumbnail_alt; ?>">
+					<a href="<?= $theme_url ?>">
+					<img class="w-full h-full object-cover" src="<?= $thumbnail_url ?>" alt="<?= $thumbnail_alt ?>">
 					</a>
 				</div>
 			</div>
@@ -31,7 +31,7 @@ $theme_url = get_the_permalink($theme);
 					<!-- Theme or format -->
 					<?php if ($secondary_color): ?>
 					<div class="topline">
-						<span style="color:<?php echo $secondary_color; ?>;">
+						<span style="color:<?= $secondary_color ?>;">
 							<?= __('Theme', BB_TEXT_DOMAIN) ?>
 						</span>
 					</div>
@@ -53,9 +53,9 @@ $theme_url = get_the_permalink($theme);
 
 				<!-- Button and extra info -->
 				<div class="flex-1 flex items-end lg:pb-8 default-scheme">
-					<a href="<?php echo $theme_url; ?>" class="btn btn-hollow btn-icon-left">
-						<?= bb_icon('arrow-right',''); ?>
-						<?= __('Zum Thema', BB_TEXT_DOMAIN) ?>						
+					<a href="<?= $theme_url ?>" class="btn btn-hollow btn-icon-left">
+						<?= bb_icon('arrow-right', '') ?>
+						<?= __('Zum Thema', BB_TEXT_DOMAIN) ?>
 					</a>
 				</div>
 
@@ -66,61 +66,41 @@ $theme_url = get_the_permalink($theme);
 			<?php if (have_rows('related_links')): ?>
 				<div class="col-span-12 my-10">
 				<div class="lg:grid lg:grid-cols-3 px-5 lg:px-0 gap-10">
-				<?php while (have_rows('related_links')):
-    	the_row(); ?>
-					<?php $link = get_sub_field('link'); ?>
-					<?php if ($link): ?>
+
+				<?php while (have_rows('related_links')): ?>
+    			<?php the_row(); ?>
+
+					<?php if ($link = get_sub_field('link')): ?>
+
 						<?php if ($secondary_color): ?>
-							<a href="<?php echo esc_url($link['url']); ?>" target="<?php echo esc_attr($link['target']); ?>">
-							<?php if (get_sub_field('alt_meta_info')): ?>
-							<?php endif; ?>
-
-							<?php if (get_sub_field('alt_meta_info')): ?>
-
-							<p class="topline mb-0" style="color:<?php echo $secondary_color; ?>;">
-								<?php the_sub_field('alt_meta_info'); ?>
-							</p>
-							<h3 class="text-base lg:text-xl text-black hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
-							<?php echo esc_html($link['title']); ?>
-							</h3>
-							<?php else: ?>
-
-							<h3 class="text-base lg:text-xl text-black mt-5 hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
-							<?php echo esc_html($link['title']); ?>
-							</h3>
-
-							<?php endif; ?>
-
+							<a href="<?= esc_url($link['url']) ?>" target="<?= esc_attr($link['target']) ?>">
+								<?php if (get_sub_field('alt_meta_info')): ?>
+									<p class="topline mb-0" style="color:<?= $secondary_color ?>;"><?php the_sub_field('alt_meta_info'); ?></p>
+									<h3 class="text-base lg:text-xl text-black hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
+										<?= esc_html($link['title']) ?>
+									</h3>
+								<?php else: ?>
+									<h3 class="text-base lg:text-xl text-black mt-5 hover:underline transition underline-offset-2 decoration-1 decoration-black transition"><?= esc_html($link['title']) ?></h3>
+								<?php endif; ?>
 							</a>
 
 						<?php else: ?>
-							<a href="<?php echo esc_url($link['url']); ?>" target="<?php echo esc_attr($link['target']); ?>">
-							<?php if (get_sub_field('alt_meta_info')): ?>
-							<?php endif; ?>
-
-							<?php if (get_sub_field('alt_meta_info')): ?>
-
-							<p class="topline mb-0">
-								<?php the_sub_field('alt_meta_info'); ?>
-							</p>
-							<h3 class="text-base lg:text-xl text-black hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
-							<?php echo esc_html($link['title']); ?>
-							</h3>
-							<?php else: ?>
-
-							<h3 class="text-base lg:text-xl text-black mt-5 hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
-							<?php echo esc_html($link['title']); ?>
-							</h3>
-
-							<?php endif; ?>
-
+							<a href="<?= esc_url($link['url']) ?>" target="<?= esc_attr($link['target']) ?>">
+								<?php if (get_sub_field('alt_meta_info')): ?>
+									<p class="topline mb-0"><?php the_sub_field('alt_meta_info'); ?></p>
+									<h3 class="text-base lg:text-xl text-black hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
+										<?= esc_html($link['title']) ?>
+									</h3>
+								<?php else: ?>
+									<h3 class="text-base lg:text-xl text-black mt-5 hover:underline transition underline-offset-2 decoration-1 decoration-black transition">
+										<?= esc_html($link['title']) ?>
+									</h3>
+								<?php endif; ?>
 							</a>
-
 						<?php endif; ?>
 
 					<?php endif; ?>
-				<?php
-    endwhile; ?>
+				<?php endwhile; ?>
 				</div>
 				</div>
 			<?php endif; ?>
