be221ecf4e1065bfccb423872bbf987fa7954c6d
Ingo Schmid <is@bananenbiegerei.de>
2023-06-08 15:36:39 +0200

    iPad RD Footer

--- footer.php
+++ footer.php
@@ -2,26 +2,28 @@
 <footer class="bg-white text-primary mt-36 site-footer" aria-labelledby="footer-heading">
 	<h2 id="footer-heading" class="sr-only">Footer</h2>
 	<div class="border-t-2 border-b border-b-gray-200 py-8 mb-12 lg:mb-0">
-		<div class="container lg:flex lg:flex-wrap lg:gap-20">
-			<div class="flex-none">
+		<div class="container grid grid-cols-1 sm:grid-cols-2 md:flex md:flex-wrap gap-10 xl:gap-20">
+			<div class="md:flex-none">
 				<?php get_template_part("template-parts/social-media-menu"); ?>
 			</div>
+			<?php /* 
 			<div  class="flex-none">
 				<?php //get_template_part("template-parts/newsletter-signup-form-minimal"); ?>
 			</div>
+			*/ ?>
 			<?php if( get_field('kontakt_spendenservice', 'option') ): ?>
-				<div  class="flex-none font-alt">
+				<div  class="md:flex-none font-alt">
 					<h3 class="text-base text-primary"><?= _e('Spendenservice', BB_TEXT_DOMAIN) ?></h3>
 					<?php the_field('kontakt_spendenservice', 'option'); ?>
 				</div>
 			<?php endif; ?>
 			<?php if( get_field('kontakt', 'option') ): ?>
-				<div  class="flex-none font-alt">
+				<div  class="md:flex-none font-alt">
 					<h3 class="text-base text-primary"><?= _e('Kontakt', BB_TEXT_DOMAIN) ?></h3>
 					<?php the_field('kontakt', 'option'); ?>
 				</div>
 			<?php endif; ?>
-			<div class="flex-1 flex justify-end items-start">
+			<div class="md:flex-1 flex justify-start xl:justify-end items-start">
 				<?php echo do_shortcode('[cookie_settings]'); ?>
 			</div>
 		</div>
@@ -31,7 +33,7 @@
 			<?php wp_nav_menu(array(
 			'container' => 'nav',
 			'menu' => 'footer',
-			'menu_class' => 'flex flex-col lg:flex-row gap-5',
+			'menu_class' => 'flex flex-col md:flex-row gap-5',
 			'theme_location' => 'footer',
 			)); ?>
 		</div>
