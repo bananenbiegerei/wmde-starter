ec1f3bfcb3f06f13804a53272fa45a1279816d4b
Éric Leclercq <eric@naad.io>
2023-07-12 14:49:06 +0200

    Menus (top & footer) are now loaded from main site. This prevents the need for double admin…

--- footer.php
+++ footer.php
@@ -1,46 +1,41 @@
 </main>
 <footer class="bg-white text-primary mt-36 site-footer" aria-labelledby="footer-heading">
-	<h2 id="footer-heading" class="sr-only">Footer</h2>
-	<div class="border-t-2 border-b border-b-gray-200 py-8 mb-12 lg:mb-0">
-		<div class="container grid grid-cols-1 sm:grid-cols-2 md:flex md:flex-wrap gap-10 xl:gap-20">
-			<div class="md:flex-none">
-				<?php get_template_part("template-parts/social-media-menu"); ?>
-			</div>
-			<?php /* 
-			<div  class="flex-none">
-				<?php //get_template_part("template-parts/newsletter-signup-form-minimal"); ?>
-			</div>
-			*/ ?>
-			<?php if( get_field('kontakt_spendenservice', 'option') ): ?>
-				<div  class="md:flex-none font-alt">
-					<h3 class="text-base text-primary"><?= _e('Spendenservice', BB_TEXT_DOMAIN) ?></h3>
-					<?php the_field('kontakt_spendenservice', 'option'); ?>
-				</div>
-			<?php endif; ?>
-			<?php if( get_field('kontakt', 'option') ): ?>
-				<div  class="md:flex-none font-alt">
-					<h3 class="text-base text-primary"><?= _e('Kontakt', BB_TEXT_DOMAIN) ?></h3>
-					<?php the_field('kontakt', 'option'); ?>
-				</div>
-			<?php endif; ?>
-			<div class="md:flex-1 flex justify-start xl:justify-end items-start">
-				<?php echo do_shortcode('[cookie_settings]'); ?>
-			</div>
-		</div>
-	</div>
-	<div class="container lg:flex lg:items-center lg:h-24">
-		<div class="lg:flex-1">
-			<?php wp_nav_menu(array(
-			'container' => 'nav',
-			'menu' => 'footer',
-			'menu_class' => 'flex flex-col md:flex-row gap-5',
-			'theme_location' => 'footer',
-			)); ?>
-		</div>
-		<div class="lg:flex-none py-12 lg:py-0">
-			<h3 class="mb-0 text-base">Wir befreien Wissen</h3>
-		</div>
-	</div>
+  <h2 id="footer-heading" class="sr-only">Footer</h2>
+  <div class="border-t-2 border-b border-b-gray-200 py-8 mb-12 lg:mb-0">
+  <div class="container grid grid-cols-1 sm:grid-cols-2 md:flex md:flex-wrap gap-10 xl:gap-20">
+    <div class="md:flex-none">
+    <?php get_template_part("template-parts/social-media-menu"); ?>
+    </div>
+    <?php /*
+  <div  class="flex-none">
+  <?php //get_template_part("template-parts/newsletter-signup-form-minimal"); ?>
+  </div>
+  */ ?>
+  <?php if(get_field('kontakt_spendenservice', 'option')): ?>
+  <div class="md:flex-none font-alt">
+    <h3 class="text-base text-primary"><?= _e('Spendenservice', BB_TEXT_DOMAIN) ?></h3>
+    <?php the_field('kontakt_spendenservice', 'option'); ?>
+  </div>
+  <?php endif; ?>
+  <?php if(get_field('kontakt', 'option')): ?>
+  <div class="md:flex-none font-alt">
+    <h3 class="text-base text-primary"><?= _e('Kontakt', BB_TEXT_DOMAIN) ?></h3>
+    <?php the_field('kontakt', 'option'); ?>
+  </div>
+  <?php endif; ?>
+  <div class="md:flex-1 flex justify-start xl:justify-end items-start">
+    <?php echo do_shortcode('[cookie_settings]'); ?>
+  </div>
+  </div>
+  </div>
+  <div class="container lg:flex lg:items-center lg:h-24">
+  <div class="lg:flex-1">
+    <?php bb_wp_nav_menu(['container' => 'nav', 'menu' => 'footer', 'menu_class' => 'flex flex-col md:flex-row gap-5', 'theme_location' => 'footer']); ?>
+  </div>
+  <div class="lg:flex-none py-12 lg:py-0">
+    <h3 class="mb-0 text-base">Wir befreien Wissen</h3>
+  </div>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
--- functions/menu.php
+++ functions/menu.php
@@ -2,92 +2,127 @@
 
 // Register navigation menus
 add_action('init', function () {
-	$locations = [
-		'nav' => 'Top Navigation Menu',
-		'footer' => __('Footer', BB_TEXT_DOMAIN),
-	];
-	register_nav_menus($locations);
+  $locations = [
+    'nav' => 'Top Navigation Menu',
+    'footer' => __('Footer', BB_TEXT_DOMAIN)
+  ];
+  register_nav_menus($locations);
 });
+
+
+// Replace menu editor with notice
+add_action(
+  'admin_enqueue_scripts',
+  function () {
+    wp_register_script('bb-admin', false, false, false, true);
+    wp_enqueue_script('bb-admin');
+    $script = "jQuery('.wp-admin.nav-menus-php .wrap').html('<div class=\"wrap\"><h2>Menus</h2><p>" . __('Please edit the menus on the main site.', BB_TEXT_DOMAIN)   .   "</p></div>');";
+    wp_add_inline_script('bb-admin', $script);
+  }
+);
+
+// Get the menus from the main site
+function bb_wp_nav_menu($args)
+{
+  if (is_multisite()) {
+    switch_to_blog(1);
+  }
+
+  wp_nav_menu($args);
+
+  if (is_multisite()) {
+    restore_current_blog();
+  }
+}
 
 // For new top navigation
 define('BB_NAV_MENU_FEATURED', 'Featured');
 
-function bb_get_nav_menu($location = 'nav')
+// Get menu data in a JSON structure for the dropdown menu
+function bb_get_nav_menu($location = 'nav', $use_main = true)
 {
-	$menu = wp_get_nav_menu_name($location);
-	if ($menu === '') {
-		return [];
-	}
-	$nav = [];
-	$featured_id = null;
-	$section_title_id = ['title' => null, 'id' => null];
-	$menu_array = wp_get_nav_menu_items($menu);
-	$menu_item_IDs = array_map(function ($a) {
-		return $a->ID;
-	}, $menu_array);
+  if ($use_main && is_multisite()) {
+    switch_to_blog(1);
+  }
 
-	foreach ($menu_array as $m) {
-		if (empty($m->menu_item_parent) || !in_array($m->menu_item_parent, $menu_item_IDs)) {
-			// If item has no parent, then it's a domain (top-level)
-			$domain = new stdClass();
-			$domain->ID = intval($m->object_id);
-			$domain->title = $m->title;
-			$domain->url = $m->url;
-			$domain->excerpt = wp_strip_all_tags(get_the_excerpt($m->object_id), true);
-			$domain->featured = [];
-			$domain->pages = [];
-			$domain->t_sections = [];
-			$domain->children = [];
-			$nav[] = $domain;
-		} elseif ($m->title == BB_NAV_MENU_FEATURED) {
-			// If the title is BB_NAV_MENU_FEATURED, the next items will be featured pages
-			$featured_id = $m->ID;
-		} elseif ($m->url == '#') {
-			// If the url is '#' it's a section title
-			$section_title_id = ['title' => $m->title, 'id' => $m->ID];
-			$domain->t_sections["{$m->title}***{$m->ID}"] = [];
-		} else {
-			// Otherwise it's a normal page
-			$page = new stdClass();
-			$page->ID = intval($m->object_id);
-			$page->title = $m->title;
-			$page->url = $m->url;
-			$page->domain_id = $domain->ID;
-			$domain->children[] = $page->ID;
-			if ($m->menu_item_parent == $featured_id) {
-				// If it's a featured page, add it to the domain's featured pages
-				$page->excerpt = wp_strip_all_tags(get_the_excerpt($m->object_id), true);
-				$page->thumbnail = get_the_post_thumbnail_url($m->object_id, 'medium');
-				// Projects can have a logo ACF (logo w/o logotype)
-				if ($logo = get_field('logo', $page->ID)) {
-					$page->logo = wp_get_attachment_url($logo);
-				}
-				$domain->featured[] = $page;
-			} elseif ($m->menu_item_parent == $section_title_id['id']) {
-				// If it's part of a section, add it to the current section
-				$domain->t_sections["{$section_title_id['title']}***{$section_title_id['id']}"][] = $page;
-			} else {
-				// Otherwise it's a normal page of that domain
-				$domain->pages[] = $page;
-			}
-		}
-	}
-	foreach ($nav as $domain) {
-		$domain->sections = [];
-		foreach ($domain->t_sections as $title => $section) {
-			$s = new stdClass();
-			$s->title = explode('***', $title)[0];
-			$s->pages = $section;
-			$domain->sections[] = $s;
-		}
-		unset($domain->t_sections);
-	}
+  $menu = wp_get_nav_menu_name($location);
+  if ($menu === '') {
+    return [];
+  }
+  $nav = [];
+  $featured_id = null;
+  $section_title_id = ['title' => null, 'id' => null];
+  $menu_array = wp_get_nav_menu_items($menu);
+  $menu_item_IDs = array_map(function ($a) {
+    return $a->ID;
+  }, $menu_array);
 
-	return $nav;
+  foreach ($menu_array as $m) {
+    if (empty($m->menu_item_parent) || !in_array($m->menu_item_parent, $menu_item_IDs)) {
+      // If item has no parent, then it's a domain (top-level)
+      $domain = new stdClass();
+      $domain->ID = intval($m->object_id);
+      $domain->title = $m->title;
+      $domain->url = $m->url;
+      $domain->excerpt = wp_strip_all_tags(get_the_excerpt($m->object_id), true);
+      $domain->featured = [];
+      $domain->pages = [];
+      $domain->t_sections = [];
+      $domain->children = [];
+      $nav[] = $domain;
+    } elseif ($m->title == BB_NAV_MENU_FEATURED) {
+      // If the title is BB_NAV_MENU_FEATURED, the next items will be featured pages
+      $featured_id = $m->ID;
+    } elseif ($m->url == '#') {
+      // If the url is '#' it's a section title
+      $section_title_id = ['title' => $m->title, 'id' => $m->ID];
+      $domain->t_sections["{$m->title}***{$m->ID}"] = [];
+    } else {
+      // Otherwise it's a normal page
+      $page = new stdClass();
+      $page->ID = intval($m->object_id);
+      $page->title = $m->title;
+      $page->url = $m->url;
+      $page->domain_id = $domain->ID;
+      $domain->children[] = $page->ID;
+      if ($m->menu_item_parent == $featured_id) {
+        // If it's a featured page, add it to the domain's featured pages
+        $page->excerpt = wp_strip_all_tags(get_the_excerpt($m->object_id), true);
+        $page->thumbnail = get_the_post_thumbnail_url($m->object_id, 'medium');
+        // Projects can have a logo ACF (logo w/o logotype)
+        if ($logo = get_field('logo', $page->ID)) {
+          $page->logo = wp_get_attachment_url($logo);
+        }
+        $domain->featured[] = $page;
+      } elseif ($m->menu_item_parent == $section_title_id['id']) {
+        // If it's part of a section, add it to the current section
+        $domain->t_sections["{$section_title_id['title']}***{$section_title_id['id']}"][] = $page;
+      } else {
+        // Otherwise it's a normal page of that domain
+        $domain->pages[] = $page;
+      }
+    }
+  }
+  foreach ($nav as $domain) {
+    $domain->sections = [];
+    foreach ($domain->t_sections as $title => $section) {
+      $s = new stdClass();
+      $s->title = explode('***', $title)[0];
+      $s->pages = $section;
+      $domain->sections[] = $s;
+    }
+    unset($domain->t_sections);
+  }
+
+  if ($use_main && is_multisite()) {
+    restore_current_blog();
+  }
+
+  return $nav;
 }
 
 function is_current_menu($id)
 {
-	global $post;
-	echo $post->ID == get_post_meta(intval($id), '_menu_item_object_id', true) ? 'current' : '';
+  global $post;
+  echo $post->ID == get_post_meta(intval($id), '_menu_item_object_id', true) ? 'current' : '';
 }
