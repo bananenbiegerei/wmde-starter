9183dccbc15696d8a454b857092d56ddeee71221
Éric Leclercq <eric@naad.io>
2023-08-17 15:51:37 +0200

    Changed twitter/X logo

--- template-parts/social-media-menu.php
+++ template-parts/social-media-menu.php
@@ -16,10 +16,18 @@
   <?php if ($url = get_field('social_twitter', 'options')): ?>
   <li>
   <a class="btn btn-ghost btn-icon-only" href="<?= $url ?>" target="_blank">
-    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" preserveAspectRatio="xMidYMid meet" viewBox="0 0 1024 1024">
-    <path fill="currentColor" d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448s448-200.6 448-448S759.4 64 512 64zm215.3 337.7c.3 4.7.3 9.6.3 14.4c0 146.8-111.8 315.9-316.1 315.9c-63 0-121.4-18.3-170.6-49.8c9 1 17.6 1.4 26.8 1.4c52 0 99.8-17.6 137.9-47.4c-48.8-1-89.8-33-103.8-77c17.1 2.5 32.5 2.5 50.1-2a111 111 0 0 1-88.9-109v-1.4c14.7 8.3 32 13.4 50.1 14.1a111.13 111.13 0 0 1-49.5-92.4c0-20.7 5.4-39.6 15.1-56a315.28 315.28 0 0 0 229 116.1C492 353.1 548.4 292 616.2 292c32 0 60.8 13.4 81.1 35c25.1-4.7 49.1-14.1 70.5-26.7c-8.3 25.7-25.7 47.4-48.8 61.1c22.4-2.4 44-8.6 64-17.3c-15.1 22.2-34 41.9-55.7 57.6z" />
+    <svg class="w-6 h-6" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="40" height="40" rx="8" fill="currentColor" />
+    <g clip-path="url(#clip0_3220_2834)">
+      <path d="M22.5597 18.2499L30.7143 8.57141H28.7819L21.7013 16.9751L16.0461 8.57141H9.52344L18.0753 21.2792L9.52344 31.4286H11.4559L18.9332 22.554L24.9055 31.4286H31.4282L22.5597 18.2499ZM19.913 21.3912L19.0465 20.1258L12.1522 10.0568H15.1204L20.6841 18.1828L21.5506 19.4482L28.7828 30.0108H25.8147L19.913 21.3912Z" fill="white" />
+    </g>
+    <defs>
+      <clipPath id="clip0_3220_2834">
+      <rect width="21.9048" height="22.8571" fill="white" transform="translate(9.52344 8.57141)" />
+      </clipPath>
+    </defs>
     </svg>
-    <span class="sr-only">Twitter</span>
+    <span class="sr-only">X</span>
   </a>
   </li>
   <?php endif; ?>
