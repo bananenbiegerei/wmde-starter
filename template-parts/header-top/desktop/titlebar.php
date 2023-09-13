9f900ac2493b7d098fc2389b19e00b17e4f18773
Ingo Schmid <is@bananenbiegerei.de>
2023-09-13 14:21:57 +0200

    solved conflicts…
    not really sure to be honest

--- template-parts/header-top/desktop/titlebar.php
+++ template-parts/header-top/desktop/titlebar.php
@@ -1,5 +1,5 @@
-<div id="titlebar_desktop" class="border-b border-gray-200 hidden md:block nohover:hidden">
+<header id="titlebar_desktop" class="border-b border-gray-200 hidden md:block nohover:hidden">
 	<div class="bg-white container items-center py-3 mx-4 flex">
 		<?php get_template_part('template-parts/header-top/titlebar_content'); ?>
 	</div>
-</div>
+</header>
--- template-parts/header-top/main.php
+++ template-parts/header-top/main.php
@@ -1,16 +1,16 @@
 <?php $WPNav = json_encode(bb_get_nav_menu()); ?>
 
 <script>
-// Get current page ID (used to set 'current' class to menu item)
-const pageID = <?= get_the_ID() ?: 'null' ?>;
-// Get content of top-nav menu
-const WPNav = JSON.parse('<?= $WPNav ?>');
-// Default icon when featured page thumbnail is missing
-const defaultIcon = "<?= get_stylesheet_directory_uri() ?>/img/placeholders/wiki-logo-icon.png";
+  // Get current page ID (used to set 'current' class to menu item)
+  const pageID = <?php echo get_the_ID() ?: 'null' ?>;
+  // Get content of top-nav menu
+  const WPNav = JSON.parse('<?= $WPNav ?>');
+  // Default icon when featured page thumbnail is missing
+  const defaultIcon = "<?= get_stylesheet_directory_uri() ?>/img/placeholders/wiki-logo-icon.png";
 </script>
-<div class="container flex">
-	<a href="#main-content" class="sr-only focus:not-sr-only m-2">Skip to Content</a>
-</div>
+<header class="container flex">
+  <a href="#main-content" class="sr-only focus:not-sr-only m-2"><?= __('Skip to Content', BB_TEXT_DOMAIN) ?></a>
+</header>
 <?php get_template_part('template-parts/header-top/mobile/titlebar'); ?>
 <?php get_template_part('template-parts/header-top/mobile/navmenu'); ?>
 <?php get_template_part('template-parts/header-top/desktop/titlebar'); ?>
