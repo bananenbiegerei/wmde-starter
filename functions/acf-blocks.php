072378e556a6f4982f269423fa6beed04b009aec
Éric Leclercq <eric@naad.io>
2023-09-14 10:13:52 +0200

    Work on buttons

--- functions/acf-blocks.php
+++ functions/acf-blocks.php
@@ -1,48 +1,46 @@
 <?php
 
-// Define this to limit which blocks will be loaded
-//define('WMDE_ALLOWED_BB_BLOCKS', ['image', 'card']);
-
-define('WMDE_ALLOWED_WP_BLOCKS', ['core/group', 'core/column', 'core/columns', 'core/html', 'core/file', 'core/shortcode']);
-
 require_once get_template_directory() . '/blocks/init.php';
 
+// TODO: migrate all clone fields to normal fields with acf/load filters for values
+
+
 add_filter('acf/load_field/name=color_light', function ($field) {
-		$field['choices'] = [
-				'default' => 'Default',
-				'white' => 'White',
-				'gray' => 'Gray',
-				'red-50' => 'Red',
-				'green-50' => 'Green',
-				'primary-50' => 'Blue',
-				'neon' => 'Neon',
-		];
-		return $field;
+  $field['choices'] = [
+    'default' => 'Default',
+    'white' => 'White',
+    'gray' => 'Gray',
+    'red-50' => 'Red',
+    'green-50' => 'Green',
+    'primary-50' => 'Blue',
+    'neon' => 'Neon',
+  ];
+  return $field;
 });
 
 add_filter('acf/load_field/name=color_dark', function ($field) {
-		$field['choices'] = [
-				'default' => 'Default',
-				'black' => 'Black',
-				'gray-700' => 'Gray',
-				'red' => 'Red',
-				'green-700' => 'Green',
-				'primary' => 'Blue',
-				'neon-800' => 'Neon',
-		];
-		return $field;
+  $field['choices'] = [
+    'default' => 'Default',
+    'black' => 'Black',
+    'gray-700' => 'Gray',
+    'red' => 'Red',
+    'green-700' => 'Green',
+    'primary' => 'Blue',
+    'neon-800' => 'Neon',
+  ];
+  return $field;
 });
 
 // BG colors for team members
 add_filter('acf/load_field/name=team_section_color', function ($field) {
-		$field['choices'] = [
-				'palerose' => 'Rose',
-				'palepurple' => 'Purple',
-				'palepeach' => 'Peach',
-				'palecyan' => 'Cyan',
-				'palepink' => 'Pink',
-				'paleblue' => 'Blue',
-				'palegreen' => 'Green',
-		];
-		return $field;
+  $field['choices'] = [
+    'palerose' => 'Rose',
+    'palepurple' => 'Purple',
+    'palepeach' => 'Peach',
+    'palecyan' => 'Cyan',
+    'palepink' => 'Pink',
+    'paleblue' => 'Blue',
+    'palegreen' => 'Green',
+  ];
+  return $field;
 });
