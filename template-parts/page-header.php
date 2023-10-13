88f6a6fdda9f38b4b3e75b26c7fade86b0cea2c6
Ingo Schmid <is@bananenbiegerei.de>
2023-10-13 13:59:28 +0200

    cta buttons in header new

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -9,23 +9,32 @@
 					<?php echo strip_tags(get_the_excerpt()); ?>
 				  </div>
 				<?php endif; ?>
+				
+		  </div>
+		<div class="col-span-12 lg:col-span-8 lg:col-start-3 relative">
+			<?php get_template_part('template-parts/featured-image'); ?>
+			<div class="absolute bottom-4 left-16">
 				<?php if (have_rows('call_to_actions_in_header')): ?>
 					  <div class="mb-10">
-					  <?php while (have_rows('call_to_actions_in_header')):
-       	the_row(); ?>
-						  <?php $cta_link = get_sub_field('cta_link'); ?>
-						  <?php if ($cta_link): ?>
-							  <a class="btn btn-base btn-icon-left" href="<?php echo esc_url($cta_link['url']); ?>" target="<?php echo esc_attr($cta_link['target']); ?>">
-							  <?= bb_icon('arrow-right', 'icon-base') ?>
-							  <?php echo esc_html($cta_link['title']); ?></a>
-						  <?php endif; ?>
-					  <?php
-       endwhile; ?>
+					  <?php while (have_rows('call_to_actions_in_header')): the_row(); ?>
+						<?php
+						$link = get_sub_field('link') ? get_sub_field('link') : ['title' => 'Missing Link!', 'url' => '#', 'target' => '_self'];
+						$icon = get_sub_field('display')['icon'];
+						$color = get_sub_field('display')['color_dark'] ?? 'primary';
+						if ($color == 'default') {
+							$color = 'primary';
+						}
+						?>
+						<div class="flex">
+						<a class="btn btn-<?= $color ?> <?= $icon ? 'btn-icon-left' : '' ?>" href="<?= esc_url($link['url']) ?>" target="<?= esc_attr($link['target']) ?>">
+						<?= bb_icon($icon, 'icon-sm') ?>
+						<?= esc_html($link['title']) ?>
+						</a>
+						</div>
+					  <?php endwhile; ?>
 					  </div>
 				  <?php endif; ?>
-		  </div>
-		<div class="col-span-12 lg:col-span-8 lg:col-start-3">
-			<?php get_template_part('template-parts/featured-image'); ?>
+			</div>
 		</div>
 	  </div>
   </div>
