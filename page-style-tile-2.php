25c94b1006251aa3c2887f8c14ca290162718ea4
Ingo Schmid <is@bananenbiegerei.de>
2023-11-01 11:19:08 +0100

    rel="me" for social links

--- page-style-tile-2.php
+++ page-style-tile-2.php
@@ -1,6 +1,7 @@
 <?php get_header(); ?>
 
 <div class="container">
+    <?php get_template_part("template-parts/style-tile/spaces"); ?>
     <?php get_template_part("template-parts/style-tile/typography"); ?>
     <?php get_template_part("template-parts/style-tile/buttons"); ?>
     <?php get_template_part("template-parts/style-tile/forms"); ?>
---
+++ template-parts/style-tile/spaces.php
@@ -0,0 +1,63 @@
+<div class="p-4">
+        <h1 class="text-xl font-semibold">Screen Sizes</h1>
+        <table class="table-fixed w-full">
+            <thead>
+                <tr>
+                    <th class="w-1/2 py-2 px-4 bg-gray-800 text-white">Screen Size</th>
+                    <th class="w-1/2 py-2 px-4 bg-gray-800 text-white">Breakpoint (Width)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-lime-500 hidden xxs:block xs-block">
+                    <td>Extra Extra Small (xxs)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        120px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>Extra Small (xs)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        320px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>Small (sm)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        640px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>Medium (md)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        768px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>Large (lg)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        1024px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>Extra Large (xl)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        1280px
+                    </td>
+                </tr>
+                <tr class="bg-X-500 hidden xxs:block xs:hidden">
+                    <td>2 Extra Large (2xl)</td>
+                    <td class="breakpoint-width">
+                        <div class="breakpoint-bar"></div>
+                        1536px
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
