13de4f25098e843fddde7cbe869038aff1d97604
Éric Leclercq <eric@naad.io>
2024-01-04 16:26:38 +0100

    Quick hack to fix featured images captions on touch devices

--- template-parts/featured-image.php
+++ template-parts/featured-image.php
@@ -1,10 +1,10 @@
-<div class="bb-image-block aspect-w-16 aspect-h-9 bg-gray-200 rounded-t-3xl">
-	<figure class="w-full w-full">
-		<?php the_post_thumbnail('large', ['class' => 'rounded-t-3xl object-cover w-full h-full overflow-hidden']); ?>
-		<?php if (bbWikimediaCommonsMedia::has_post_thumbnail_caption()): ?>
-			<figcaption class="invisible flex absolute left-0 bottom-0 right-0 text-white bg-gray-900 w-auto h-auto z-20 p-2 text-sm flex items-start gap-4 break-all">
-				<?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
-			</figcaption>
-		<?php endif; ?>
-	</figure>
+<div class="bb-image-block bg-gray-200 aspect-w-16 aspect-h-9 rounded-t-3xl nohover:mb-16">
+  <figure class="w-full w-full">
+  <?php the_post_thumbnail('large', ['class' => 'rounded-t-3xl object-cover w-full h-full overflow-hidden']); ?>
+  <?php if (bbWikimediaCommonsMedia::has_post_thumbnail_caption()): ?>
+  <figcaption class="invisible flex absolute left-0 bottom-0 right-0 text-white bg-gray-900 w-auto h-auto z-20 p-2 text-sm flex items-start gap-4 break-all">
+    <?= bb_icon('info', 'flex-shrink-0') ?> <div class="self-center"><?php the_post_thumbnail_caption(); ?></div>
+  </figcaption>
+  <?php endif; ?>
+  </figure>
 </div>
