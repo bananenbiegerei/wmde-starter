28f8b54eacb1c21cc6dcd8e4e2466272e1c90e9c
Ingo Schmid <is@bananenbiegerei.de>
2024-01-10 11:03:24 +0100

    feateures for big page header

--- template-parts/page-header-image.php
+++ template-parts/page-header-image.php
@@ -2,11 +2,10 @@
 $img_vertical_position = get_field('header_image_position_vertical');
 $img_horizontal_position = get_field('header_image_position_horizontal');
 $img_position = 'object-' . $img_horizontal_position . '-' . $img_vertical_position;
-var_dump($img_position);
 ?>
 <div
-    class="rounded-bl-xl rounded-br-xl bg-gray-light mb-10 h-auto lg:h-[30rem] rounded-bl-xl rounded-br-xl overflow-hidden relative text-white">
-    <div class="container py-10 grid grid-cols-1 content-between h-full">
+    class="nohover:mt-12 rounded-bl-xl rounded-br-xl bg-gray-light mb-10 h-auto lg:h-[30rem] rounded-bl-xl rounded-br-xl overflow-hidden relative text-white">
+    <div class="container py-5 lg:py-10 grid grid-cols-1 content-between h-full">
         <div class="mb-10">
             <?php $page_logo = get_field('page_logo'); ?>
             <?php $size = 'full'; ?>
@@ -25,8 +24,8 @@ var_dump($img_position);
         </div>
     </div>
     <?php if (has_post_thumbnail()) { ?>
-    <div class="absolute top-0 left-0 w-full h-full -z-10">
-        <?php the_post_thumbnail('full', ['class' => '-mb-10 object-contain h-full w-full ' . $img_position]); ?>
+    <div class="absolute top-0 left-0 w-full h-full -z-10 bg-red-500">
+        <?php the_post_thumbnail('full', ['class' => 'object-cover bg-yellow-300 w-full h-full ' . $img_position]); ?>
     </div>
     <?php } ?>
 </div>
