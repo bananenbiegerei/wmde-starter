1dc911959587aef0caaf90f0ab7283d42e068919
Ingo Schmid <is@bananenbiegerei.de>
2024-01-10 14:33:59 +0100

    disable shortcode icons

--- template-parts/header-top/desktop/search-slide-out.php
+++ template-parts/header-top/desktop/search-slide-out.php
@@ -13,9 +13,11 @@
     </div>
     <button class="btn btn-ghost ml-3" x-on:click="open = ! open; $nextTick(() => $refs.searchInput.focus())">
         <span class="sr-only">Toggle Search Input</span><?php echo bb_icon('search', 'icon-sm'); ?>
-        <?/*<span class="border rounded border-gray-200 text-gray-400 p-1 text-sm flex items-center">
+        <?php
+/* <span class="border rounded border-gray-200 text-gray-400 p-1 text-sm flex items-center">
             <?php echo bb_icon('command', 'icon-xs'); ?>
         <span>K</span>
-        </span>*/?>
+        </span> */
+?>
     </button>
 </div>
