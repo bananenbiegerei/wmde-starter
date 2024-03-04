537651c14d8dd2a16aa1e7a8986950520b98f60d
Ingo Schmid <is@bananenbiegerei.de>
2024-03-04 10:01:24 +0100

    changed swup container, to get swup on all pages

--- single-timeline-item.php
+++ single-timeline-item.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 <?php while (have_posts()): ?>
-<?php the_post(); ?>test
-<?php
-	//get_template_part('template-parts/page-header');
-	?>
-<div id="swup">
+<?php the_post(); ?>
+<div id="main-content">
     <div class="fixed top-0 left-0 w-full h-full z-40 content" id="swup-modal">
-	<div class="relative h-full w-full p-10">
-	    <a class="block absolute top-0 left-0 w-full h-full bg-black bg-opacity-60" href="/timeline/">close</a>
-	    <div class="relative max-w-2xl mx-auto z-10 bg-white border-4 border-black">
-		<?php the_content(); ?>
-	    </div>
-	</div>
+        <div class="relative h-full w-full p-10">
+            <a class="block absolute top-0 left-0 w-full h-full bg-black bg-opacity-60" href="/timeline/">close</a>
+            <div class="relative max-w-2xl mx-auto z-10 bg-white border-4 border-black">
+                <h1><?php the_title(); ?></h1>
+                <?php the_content(); ?>
+            </div>
+        </div>
     </div>
 </div>
 <?php endwhile; ?>
