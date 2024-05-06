bdd07bd0357b0b2cefc99440e798111d79a33f49
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-05-06 14:57:55 +0200

    Hard 404 redirect to certain page

--- 404.php
+++ 404.php
@@ -1,16 +1,5 @@
 <?php
-get_header(); ?>
-<div class="container">
-	<div class="flex flex-col items-center my-20">
-		<p>404</p>
-		<h1><?php _e('Seite nicht gefunden.', BB_TEXT_DOMAIN); ?></h1>
-		<p><?php _e('Leider konnten wir die gesuchte Seite nicht finden.', BB_TEXT_DOMAIN); ?></p>
-		<div class="mt-6">
-		  <form class="flex gap-5" action="<?php echo home_url('/'); ?>" method="get">
-			  <input class="!mb-0" type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
-			  <input type="submit" alt="Search" value="Suchen" class="btn" />
-		  </form>
-		</div>
-	</div>
-</div>
-<?php get_footer(); ?>
+header("HTTP/1.1 301 Moved Permanently");
+$page_id = 64416; // Title: Inhalt nicht gefunden
+header("Location: " . get_permalink($page_id));
+exit(); ?>
