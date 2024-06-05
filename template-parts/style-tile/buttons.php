3a033b441e675b23a8805f1840dddf6fac3426c7
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-06-05 10:47:07 +0200

    updated buttons styling in ui

--- template-parts/style-tile/buttons.php
+++ template-parts/style-tile/buttons.php
@@ -1,97 +1,103 @@
 <h3>Buttons</h3>
-<div class="space-y-5">
-    <h4 class="mb-0">Buttons with brand colors</h4>
-    <button class="btn">Button</button>
-    <button class="btn ">Primary</button>
-    <button class="btn btn-secondary">Secondary</button>
-    <button class="btn btn-accent">Accent</button>
-    <button class="btn btn-ghost">Ghost</button>
-    <button class="btn btn-link">Link</button>
+<div class="space-y-5" x-data="{
+	colors: ['primary', 'secondary', 'accent', 'warning', 'error', 'success', 'neutral'],
+	sizes: ['xl', 'lg', 'default', 'md', 'sm', 'xs']
+}">
+    <h4 class="mb-0">Colors</h4>
+    <div class="btn-group">
+        <template x-for="(color, index) in colors" :key="index">
+            <button :class="`btn btn-${color}`" x-text="color"></button>
+        </template>
+    </div>
     <hr>
-	<h4 class="mb-0">Active buttons</h4>
-    <button class="btn btn-active">Default</button>
-	<button class="btn btn-active ">Primary</button>
-	<button class="btn btn-active btn-secondary">Secondary</button>
-	<button class="btn btn-active btn-accent">Accent</button>
-	<button class="btn btn-active btn-ghost">Ghost</button>
-	<button class="btn btn-active btn-link">Link</button>
+    <h4 class="mb-0">State: active</h4>
+    <h4 class="mb-0">State: disabled</h4>
     <hr>
-	<h4 class="mb-0">Buttons with state colors</h4>
-	<button class="btn btn-success">Success</button>
-	<button class="btn btn-warning">Warning</button>
-	<button class="btn btn-error">Error</button>
+    <h4 class="mb-0">Ghots</h4>
+    <div class="btn-group">
+        <template x-for="(color, index) in colors" :key="index">
+            <button :class="`btn btn-ghost btn-${color}`" x-text="color"></button>
+        </template>
+    </div>
     <hr>
-	<h4 class="mb-0">Buttons Ghosts with state colors</h4>
-	<button class="btn btn-ghost btn-success">Success</button>
-	<button class="btn btn-ghost btn-warning">Warning</button>
-	<button class="btn btn-ghost btn-error">Error</button>
+    <h4 class="mb-0">Outline</h4>
+    <div class="btn-group">
+        <template x-for="(color, index) in colors" :key="index">
+            <button :class="`btn btn-outline btn-${color}`" x-text="color"></button>
+        </template>
+    </div>
     <hr>
-	<h4 class="mb-0">Outline buttons</h4>
-	<button class="btn btn-outline">Default</button>
-	<button class="btn btn-outline ">Primary</button>
-	<button class="btn btn-outline btn-secondary">Secondary</button>
-	<button class="btn btn-outline btn-accent">Accent</button>
-	<hr>
-	<h4 class="mb-0">Outline buttons with state colors</h4>
-	<button class="btn btn-outline btn-success">Success</button>
-	<button class="btn btn-outline btn-warning">Warning</button>
-	<button class="btn btn-outline btn-error">Error</button>
-	<hr>
-	<h4 class="mb-0">Button sizes</h4>
-	<button class="btn btn-xl">XLarge</button>
-	<button class="btn btn-lg">Large</button>
-	<button class="btn">Normal</button>
-	<button class="btn btn-sm">Small</button>
-	<button class="btn btn-xs">Tiny</button>
-	<hr>
-	<h4 class="mb-0">Button sizes with icons before</h4>
-	<button class="btn btn-xl">
-		<span class="bb-icon icon-xl"><!--?xml version="1.0" encoding="utf-8"?-->
-			<svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
-  				<path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z" fill="currentColor">
-				</path>
-</svg></span>
-		XLarge
-	</button>
-	<button class="btn btn-lg">
-		<span class="bb-icon icon-lg"><!--?xml version="1.0" encoding="utf-8"?-->
-<svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
-  <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z" fill="currentColor"></path>
-</svg></span>
-		Large
-	</button>
-	<button class="btn">
-	<span class="bb-icon icon-base"><!--?xml version="1.0" encoding="utf-8"?-->
-<svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
-  <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z" fill="currentColor"></path>
-</svg></span>
-	Normal
-</button>
-	<button class="btn btn-sm">
-	<span class="bb-icon icon-sm"><!--?xml version="1.0" encoding="utf-8"?-->
-<svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
-  <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z" fill="currentColor"></path>
-</svg></span>
-	Small
-</button>
-	<button class="btn btn-xs">
-	<span class="bb-icon icon-xs"><!--?xml version="1.0" encoding="utf-8"?-->
-<svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
-  <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z" fill="currentColor"></path>
-</svg></span>
-	Tiny
-</button>
-	<hr>
-	<h4 class="mb-0">Buttons with different HTML Tags</h4>
-	<code class="bg-warning-100 rounded block p-2">
+    <h4 class="mb-0">Sizes</h4>
+    <div class="btn-group">
+        <template x-for="(size, index) in sizes" :key="index">
+            <button :class="`btn btn-primary btn-${size}`" x-text="`Button ${size}`"></button>
+        </template>
+    </div>
+    <hr>
+    <h4 class="mb-0">Sizes with icons before</h4>
+
+    <button class="btn btn-xl">
+        <span class="bb-icon icon-xl">
+            <!--?xml version="1.0" encoding="utf-8"?-->
+            <svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
+                <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z"
+                    fill="currentColor">
+                </path>
+            </svg>
+        </span>
+        XLarge
+    </button>
+    <button class="btn btn-lg">
+        <span class="bb-icon icon-lg">
+            <!--?xml version="1.0" encoding="utf-8"?-->
+            <svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
+                <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z"
+                    fill="currentColor"></path>
+            </svg>
+        </span>
+        Large
+    </button>
+    <button class="btn">
+        <span class="bb-icon icon-base">
+            <!--?xml version="1.0" encoding="utf-8"?-->
+            <svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
+                <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z"
+                    fill="currentColor"></path>
+            </svg>
+        </span>
+        Normal
+    </button>
+    <button class="btn btn-sm">
+        <span class="bb-icon icon-sm">
+            <!--?xml version="1.0" encoding="utf-8"?-->
+            <svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
+                <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z"
+                    fill="currentColor"></path>
+            </svg>
+        </span>
+        Small
+    </button>
+    <button class="btn btn-xs">
+        <span class="bb-icon icon-xs">
+            <!--?xml version="1.0" encoding="utf-8"?-->
+            <svg width="20" height="20" viewBox="0 0 20 20" role="presentation" aria-hidden="true">
+                <path d="M17 9.96296L12.1601 17H10.3128L14.4877 10.7407H2V9.22222H14.4877L10.3128 3H12.1601L17 9.96296Z"
+                    fill="currentColor"></path>
+            </svg>
+        </span>
+        Tiny
+    </button>
+    <hr>
+    <h4 class="mb-0">Buttons with different HTML Tags</h4>
+    <code class="bg-warning-100 rounded block p-2">
 		Radio and checkbox not refinded yet. Because of complications with forms.
 		Let's see if we need it.
 	</code>
-	<a role="button" class="btn">Link</a>
-	<button type="submit" class="btn">Button</button>
-	<input type="button" value="Input" class="btn" />
-	<input type="submit" value="Submit" class="btn" />
-	<input type="radio" aria-label="Radio" class="btn" />
-	<input type="checkbox" aria-label="Checkbox" class="btn" />
-	<input type="reset" value="Reset" class="btn" />
+    <a role="button" class="btn">Link</a>
+    <button type="submit" class="btn">Button</button>
+    <input type="button" value="Input" class="btn" />
+    <input type="submit" value="Submit" class="btn" />
+    <input type="radio" aria-label="Radio" class="btn" />
+    <input type="checkbox" aria-label="Checkbox" class="btn" />
+    <input type="reset" value="Reset" class="btn" />
 </div>
