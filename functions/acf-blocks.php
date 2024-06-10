8245a15c9c270c0166e370ca5d7076ed044750df
Éric Leclercq <eric@naad.io>
2024-06-10 14:10:56 +0200

    (…)

--- functions/acf-blocks.php
+++ functions/acf-blocks.php
@@ -1,15 +1,5 @@
 <?php
 
-// Check if ACF Pro is installed
-if (!function_exists('acf_register_block_type')) {
-    add_action('admin_notices', function () {
-        echo '<div class="notice notice-error"><p><b>WMDE Starter Theme:</b> ' .
-          __('Required plugin "Advanced Custom Fields" is missing. Please install and activate.', BB_TEXT_DOMAIN) .
-          '</p></div>';
-    });
-    return;
-}
-
 // To override the options and force loading all of the blocks and force choosing default blocks
 define('BB_LOAD_ALL_BB_BLOCKS', false);
 define('BB_ALLOWED_WP_BLOCKS', []);
