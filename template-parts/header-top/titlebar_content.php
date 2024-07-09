151f78bda51b66a6f7a853bab920aac8a21cb7d7
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-07-09 15:29:15 +0200

    call to actions in a more global way

--- template-parts/header-top/titlebar_content.php
+++ template-parts/header-top/titlebar_content.php
@@ -6,7 +6,7 @@ $logo_small = esc_attr(get_field('logo_small', 'options') ?: get_stylesheet_dire
 
 
 <div class="flex w-full items-center">
-    check if logos exist else button to admin
+
     <div>
         <a href="<?= $home_url ?>" class="hidden md:block">
             <img class="logo" style="max-height: 41px" src="<?= $logo_big ?>" alt="Wikimedia Logo">
@@ -18,20 +18,16 @@ $logo_small = esc_attr(get_field('logo_small', 'options') ?: get_stylesheet_dire
 
     <div class="flex justify-end grow gap-5">
         <ul class="flex items-center space-x-2 md:space-x-5 mb-0">
-            <?php if (get_field('link_fur_spenden', 'options')): ?>
-            <li>
-                <a class="btn btn-error btn-outline" href="<?php echo esc_url(get_field('link_fur_spenden', 'option')); ?>">
-                    <?= bb_icon('heart', 'heartbeat icon-sm') ?>
-                    <?php _e('Spenden', BB_TEXT_DOMAIN); ?>
-                </a>
-            </li>
+            <?php if ( have_rows( 'call_to_actions', 'option' ) ) : ?>
+            <?php while ( have_rows( 'call_to_actions', 'option' ) ) : the_row(); ?>
+            <?php $link = get_sub_field( 'link' ); ?>
+            <?php $link_color = get_sub_field( 'color' );?>
+            <?php if ( $link ) : ?>
+            <a class="btn btn-<?php echo $link_color; ?>" href="<?php echo esc_url( $link['url'] ); ?>"
+                target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?></a>
             <?php endif; ?>
-            <?php if (get_field('link_fur_mitmachen', 'options')): ?>
-            <li class="hidden md:block">
-                <a class="btn btn-ghost" href="<?php echo esc_url(get_field('link_fur_mitmachen', 'option')); ?>">
-                    <?php _e('Mitmachen', BB_TEXT_DOMAIN); ?>
-                </a>
-            </li>
+
+            <?php endwhile; ?>
             <?php endif; ?>
         </ul>
     </div>
