05f76fecdd833be90df45f4fc720f576380701d2
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2024-07-10 11:01:16 +0200

    refined and mobile ctas

--- template-parts/header-top/titlebar_content.php
+++ template-parts/header-top/titlebar_content.php
@@ -6,8 +6,7 @@ $logo_small = esc_attr(get_field('logo_small', 'options') ?: get_stylesheet_dire
 
 
 <div class="flex w-full items-center">
-
-    <div>
+    <div class="flex-1">
         <a href="<?= $home_url ?>" class="hidden md:block">
             <img class="logo" style="max-height: 41px" src="<?= $logo_big ?>" alt="Wikimedia Logo">
         </a>
@@ -16,18 +15,7 @@ $logo_small = esc_attr(get_field('logo_small', 'options') ?: get_stylesheet_dire
         </a>
     </div>
 
-    <div class="flex justify-end grow gap-5">
-        <ul class="flex items-center space-x-2 md:space-x-5 mb-0">
-            <?php if ( have_rows( 'call_to_actions', 'option' ) ) : ?>
-            <?php while ( have_rows( 'call_to_actions', 'option' ) ) : the_row(); ?>
-            <?php $link = get_sub_field( 'link' ); ?>
-            <?php $link_color = get_sub_field( 'color' );?>
-            <?php if ( $link ) : ?>
-            <a class="btn btn-<?php echo $link_color; ?>" href="<?php echo esc_url( $link['url'] ); ?>"
-                target="<?php echo esc_attr( $link['target'] ); ?>"><?php echo esc_html( $link['title'] ); ?></a>
-            <?php endif; ?>
-            <?php endwhile; ?>
-            <?php endif; ?>
-        </ul>
+    <div class="hidden md:block">
+        <?php get_template_part('template-parts/header-top/cta'); ?>
     </div>
 </div>
