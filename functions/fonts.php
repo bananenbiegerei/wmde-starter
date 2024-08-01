461967c9a65d07d9de6154e3aea8e8d45ca6b5d7
Éric Leclercq <eric@naad.io>
2024-08-01 13:42:59 +0200

    Added support for fonts uploaded to Media Library

--- functions/fonts.php
+++ functions/fonts.php
@@ -1,31 +1,67 @@
 <?php
 
 /* Todo
-- support uploading fonts?
 - add support for different styles (normal / italic ?)
 */
 
+add_filter('wp_check_filetype_and_ext', function ($data, $file, $filename, $mimes, $real_mime) {
+    if (! empty($data['ext']) && ! empty($data['type'])) {
+        return $data;
+    }
+    $wp_file_type = wp_check_filetype($filename, $mimes);
+    switch ($wp_file_type['ext']) {
+        case 'ttf':
+            $data['ext'] = 'ttf';
+            $data['type'] = 'font/ttf';
+            break;
+        case 'woff':
+            $data['ext'] = 'woff';
+            $data['type'] = 'font/woff';
+            break;
+        case 'woff2':
+            $data['ext'] = 'woff2';
+            $data['type'] = 'font/woff2';
+            break;
+    }
+    return $data;
+}, 10, 5);
 
-define('BB_FONTS_DIR', 'fonts');
+
+add_filter('upload_mimes', function ($mimes) {
+    $mimes['ttf']   = 'font/ttf';
+    $mimes['woff']  = 'font/woff';
+    $mimes['woff2'] = 'font/woff2';
+    return $mimes;
+});
 
 // Debug stuff...
 add_action('xinit', function () {
-    // echo '<pre>';
-    // var_dump(get_fields('options'));
-    // echo '</pre>';
-    // die;
+    echo '<pre>';
+    var_dump(get_fields('options'));
+    echo '</pre>';
+    die;
 });
 
 
 function bb_list_available_fonts()
 {
-    $fonts = glob(get_template_directory() . '/'. BB_FONTS_DIR . '/*');
-    $fonts = array_map('basename', $fonts);
+    $fonts = [];
+    $args = array('post_type'=>'attachment','numberposts'=>null,'post_status'=>null);
+    $attachments = get_posts($args);
+    foreach ($attachments as $attachment) {
+        if (!str_starts_with($attachment->post_mime_type, 'font/')) {
+            continue;
+        }
+        $fonts[] = wp_get_attachment_url($attachment->ID);
+    }
     return $fonts;
 }
 
 add_filter('acf/load_field/name=custom_font_file', function ($field) {
-    $field['choices'] = bb_list_available_fonts();
+    $field['choices'] =[];
+    foreach (bb_list_available_fonts() as $font) {
+        $field['choices'][$font] = basename($font);
+    }
     return $field;
 });
 
@@ -43,24 +79,22 @@ function bb_get_font_format($filename)
 
 function bb_inline_style_fonts()
 {
-    $theme_uri = get_template_directory_uri();
     $bb_fonts_css = "/* Custom Fonts */\n";
     foreach ([ 'h' => 'Headings', 't' => 'Texts'] as $k => $l) {
         while (have_rows('fonts_' . $k, 'options')) {
             the_row();
-            $filename = get_sub_field('custom_font_file');
+            $name = get_sub_field('custom_font_file')['label'];
+            $uri = get_sub_field('custom_font_file')['value'];
             $weight = get_sub_field('custom_font_weight');
             $weight = $weight == 'variable' ? '100 700' : $weight;
-            $format = bb_get_font_format($filename);
-            $bb_fonts_css .= "
-                @font-face {
-                    font-family: '{$l}';
-                    font-style: normal;
-                    font-weight: {$weight};
-                    font-display: swap;
-                    src: url('{$theme_uri}/fonts/{$filename}') format('{$format}');
-                }";
-            $bb_fonts_css .= "\n";
+            $format = bb_get_font_format(basename($uri));
+            $bb_fonts_css .= "@font-face {\n";
+            $bb_fonts_css .= "    font-family: '{$l}';\n";
+            $bb_fonts_css .= "    font-style: normal;\n";
+            $bb_fonts_css .= "    font-weight: {$weight};\n";
+            $bb_fonts_css .= "    font-display: swap;\n";
+            $bb_fonts_css .= "    src: url('{$uri}') format('{$format}');\n";
+            $bb_fonts_css .= "}\n";
         }
     }
     return $bb_fonts_css;
@@ -70,9 +104,7 @@ function bb_inline_style_fonts()
 add_action(
     'wp_enqueue_scripts',
     function () {
-        // if (get_field('use_custom_colors', 'options')) {
         wp_add_inline_style('style', bb_inline_style_fonts());
-    // }
     },
     PHP_INT_MAX
 );
