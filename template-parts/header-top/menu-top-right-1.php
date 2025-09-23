b10dc3a3e7a723bbd95a695920b11dd91e4392e5
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2025-09-23 14:16:15 +0200

    fixes for buttons and nav colors

--- template-parts/header-top/menu-top-right-1.php
+++ template-parts/header-top/menu-top-right-1.php
@@ -2,7 +2,7 @@
 wp_nav_menu( array(
   'theme_location' => 'nav-right-level-1',
   'menu_id'        => 'nav-right-level-1',
-  'menu_class'     => 'menu horizontal right dynamic-text-color subnav', // Add the 'menu' class to the ul element
+  'menu_class'     => 'menu horizontal right', // Add the 'menu' class to the ul element
   'container'      => 'div',  // Wrap the menu in a div
   'container_class'=> 'nav-container', // Add a class to the container div
 ) );
--- template-parts/header-top/mobile/titlebar.php
+++ template-parts/header-top/mobile/titlebar.php
@@ -21,9 +21,9 @@ document.addEventListener('alpine:init', () => {
 
 <header aria-hidden="true" tabindex='-1' id="titlebar_mobile" class="flex bg-<?= $header_color; ?> h-14 items-center py-1 px-5 left-0 right-0 fixed  border-b border-neutral-light z-40  md:hidden nohover:flex">
   <?php get_template_part('template-parts/header-top/titlebar_content'); ?>
-  <div class="block flex-none" x-data="navMenuMobileToggle">
+  <div class="flex-none block" x-data="navMenuMobileToggle">
   <!-- Using the Alpine.store ($store) to save the state of the site header. -->
-  <button class="btn  btn-ghost btn-lg btn-icon" x-on:click="$store.open_mobile_nav = ! $store.open_mobile_nav; toggleNav()">
+  <button class="text-white btn btn-ghost btn-lg btn-icon" x-on:click="$store.open_mobile_nav = ! $store.open_mobile_nav; toggleNav()">
     <?= bb_icon('menu-alt-2', '') ?>
   </button>
   </div>
