d9e96971ee8ea7172960d5177536cb19cac57f97
bananenbiegerei <74664048+bananenbiegerei@users.noreply.github.com>
2026-03-17 10:25:13 +0100

    page layout for wikiwegsweier subsites

--- page.php
+++ page.php
@@ -1,9 +1,18 @@
 <?php get_header(); ?>
 <?php while (have_posts()): ?>
 <?php the_post(); ?>
-	<?php get_template_part('template-parts/page-header'); ?>
-	<div class="content pt-10">
-		<?php the_content(); ?>
-	</div>
+<div class="mt-10 outer-container">
+    <div class="bb-container-default rounded-t-xl">
+        <h1 class="my-5"><?php the_title(); ?></h1>
+        <?php if (has_excerpt()): ?>
+        <div class="mb-10 text-xl font-normal lg:text-2xl">
+            <?php echo strip_tags(get_the_excerpt()); ?>
+        </div>
+        <?php endif; ?>
+    </div>
+</div>
+<div class="content">
+    <?php the_content(); ?>
+</div>
 <?php endwhile; ?>
 <?php get_footer(); ?>
